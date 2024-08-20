6709904afe02cbd0d68331f5ce905d15387be3fb
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2024-08-20 10:49:21 +0200

    Moved frame check function to separate file

--- tests/unit/test_ts_l2_frame_check_input_params.c
+++ tests/unit/test_ts_l2_frame_check_input_params.c
@@ -2,11 +2,8 @@
 #include "unity.h"
 
 #include "libtropic_common.h"
+#include "lt_crc16.h"
 #include "lt_l2_frame_check.h"
-#include "lt_l2.h"
-
-#include "mock_lt_l1.h"
-#include "mock_lt_crc16.h"
 
 void setUp(void)
 {
@@ -18,7 +15,7 @@ void tearDown(void)
 
 //----------------------------------------------------------------------
 // Test if function returns expected error on non valid input parameter
-void test_lt_l2_transfer___NULL_h()
+void test_lt_l2_frame_check___NULL_frame()
 {
-    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l2_transfer(NULL));
+    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l2_frame_check(NULL));
 }
