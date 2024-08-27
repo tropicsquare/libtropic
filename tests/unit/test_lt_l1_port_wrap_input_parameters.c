7b9cf5795dbd5e6a563d5e5fa9148ea2c063bd80
Jan Medek <jan.medek@tropicsquare.com>
2024-08-27 10:13:10 +0200

    test input parameters for lt_l1_spi_{csn_{low, high}, transfer, delay}()

--- tests/unit/test_lt_l1_port_wrap_input_parameters.c
+++ tests/unit/test_lt_l1_port_wrap_input_parameters.c
@@ -2,6 +2,7 @@
 
 #include "libtropic_common.h"
 #include "lt_l1_port_wrap.h"
+#include "lt_l1.h"
 
 #include "mock_libtropic_port.h"
 
@@ -12,6 +13,7 @@ void setUp(void)
 void tearDown(void)
 {
 }
+
 //---------------------------------------------------------------------------------------------------------------------
 // Test if function returns LT_PARAM_ERR on non valid input parameter
 void test_lt_l1_init___NULL_h()
@@ -24,4 +26,32 @@ void test_lt_l1_init___NULL_h()
 void test_lt_l1_deinit___NULL_h()
 {
     TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l1_deinit(NULL));
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// Test if function returns LT_PARAM_ERR on non valid input parameter
+void test_lt_l1_spi_csn_low___NULL_h()
+{
+    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l1_spi_csn_low(NULL));
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// Test if function returns LT_PARAM_ERR on non valid input parameter
+void test_lt_l1_spi_csn_high___NULL_h()
+{
+    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l1_spi_csn_high(NULL));
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// Test if function returns LT_PARAM_ERR on non valid input parameter
+void test_lt_l1_spi_transfer___NULL_h()
+{
+    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l1_spi_transfer(NULL, 1, 1, LT_L1_TIMEOUT_MS_DEFAULT));
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+// Test if function returns LT_PARAM_ERR on non valid input parameter
+void test_lt_l1_delay___NULL_h()
+{
+    TEST_ASSERT_EQUAL(LT_PARAM_ERR, lt_l1_delay(NULL, LT_L1_TIMEOUT_MS_DEFAULT));
 }
