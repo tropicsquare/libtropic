b16210ab9dcdfe5889069186bcc323a077a97e07
Pavel Polach <pavel.polach@tropicsquare.com>
2024-08-27 15:17:59 +0200

    Update comments Change return values for port functions and update tests accordingly

--- tests/unit/test_lt_l1_port_wrap.c
+++ tests/unit/test_lt_l1_port_wrap.c
@@ -16,7 +16,7 @@ void tearDown(void)
 
 //---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_init() returns LT_FAIL when lt_port_init() function returns LT_FAIL
-void test_lt_l1_init___fail_during_lt_port_init__LT_FAIL()
+void test_lt_l1_init___lt_port_init__LT_FAIL()
 {
     lt_handle_t h = {0};
 
@@ -26,21 +26,8 @@ void test_lt_l1_init___fail_during_lt_port_init__LT_FAIL()
     TEST_ASSERT_EQUAL(LT_FAIL, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_init() returns LT_L1_SPI_ERROR when lt_port_init() function returns LT_L1_SPI_ERROR
-void test_lt_l1_init___fail_during_lt_port_init__LT_L1_SPI_ERROR()
-{
-    lt_handle_t h = {0};
-
-    lt_port_init_ExpectAndReturn(&h, LT_L1_SPI_ERROR);
-
-    lt_ret_t ret = lt_l1_init(&h);
-    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, ret);
-}
-
-//---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_init() returns LT_OK when lt_port_init() function returns LT_OK
-void test_lt_l1_init___fail_during_lt_port_init__LT_OK()
+void test_lt_l1_init____lt_port_init__LT_OK()
 {
     lt_handle_t h = {0};
 
@@ -50,9 +37,8 @@ void test_lt_l1_init___fail_during_lt_port_init__LT_OK()
     TEST_ASSERT_EQUAL(LT_OK, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_deinit() returns LT_FAIL when lt_port_deinit() function returns LT_FAIL
-void test_lt_l1_deinit___fail_during_lt_port_deinit__LT_FAIL()
+void test_lt_l1_deinit___lt_port_deinit__LT_FAIL()
 {
     lt_handle_t h = {0};
 
@@ -62,21 +48,7 @@ void test_lt_l1_deinit___fail_during_lt_port_deinit__LT_FAIL()
     TEST_ASSERT_EQUAL(LT_FAIL, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_deinit() returns LT_L1_SPI_ERROR when lt_port_deinit() function returns LT_L1_SPI_ERROR
-void test_lt_l1_deinit___fail_during_lt_port_deinit__LT_L1_SPI_ERROR()
-{
-    lt_handle_t h = {0};
-
-    lt_port_deinit_ExpectAndReturn(&h, LT_L1_SPI_ERROR);
-
-    lt_ret_t ret = lt_l1_deinit(&h);
-    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, ret);
-}
-
-//---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_deinit() returns LT_OK when lt_port_deinit() function returns LT_OK
-void test_lt_l1_deinit___fail_during_lt_port_deinit__LT_OK()
+void test_lt_l1_deinit___lt_port_deinit__LT_OK()
 {
     lt_handle_t h = {0};
 
@@ -87,20 +59,19 @@ void test_lt_l1_deinit___fail_during_lt_port_deinit__LT_OK()
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_spi_csn_low() returns LT_L1_SPI_ERROR when lt_port_spi_csn_low() function returns LT_L1_SPI_ERROR
-void test_lt_l1_spi_csn_low___fail_during_lt_port_spi_csn_low__LT_L1_SPI_ERROR()
+// Check that function lt_l1_spi_csn_low() returns LT_FAIL when lt_port_spi_csn_low() function returns LT_FAIL
+void test_lt_l1_spi_csn_low___lt_port_spi_csn_low__LT_FAIL()
 {
     lt_handle_t h = {0};
 
-    lt_port_spi_csn_low_ExpectAndReturn(&h, LT_L1_SPI_ERROR);
+    lt_port_spi_csn_low_ExpectAndReturn(&h, LT_FAIL);
 
     lt_ret_t ret = lt_l1_spi_csn_low(&h);
-    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, ret);
+    TEST_ASSERT_EQUAL(LT_FAIL, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_spi_csn_low() returns LT_OK when lt_port_spi_csn_low() function returns LT_OK
-void test_lt_l1_spi_csn_low___fail_during_lt_port_spi_csn_low__LT_OK()
+void test_lt_l1_spi_csn_low___lt_port_spi_csn_low__LT_OK()
 {
     lt_handle_t h = {0};
 
@@ -111,20 +82,19 @@ void test_lt_l1_spi_csn_low___fail_during_lt_port_spi_csn_low__LT_OK()
 }
 
 //---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_spi_csn_high() returns LT_L1_SPI_ERROR when lt_port_spi_csn_high() function returns LT_L1_SPI_ERROR
-void test_lt_l1_spi_csn_high___fail_during_lt_port_spi_csn_high__LT_L1_SPI_ERROR()
+// Check that function lt_l1_spi_csn_high() returns LT_FAIL when lt_port_spi_csn_high() function returns LT_FAIL
+void test_lt_l1_spi_csn_high___lt_port_spi_csn_high__LT_FAIL()
 {
     lt_handle_t h = {0};
 
-    lt_port_spi_csn_high_ExpectAndReturn(&h, LT_L1_SPI_ERROR);
+    lt_port_spi_csn_high_ExpectAndReturn(&h, LT_FAIL);
 
     lt_ret_t ret = lt_l1_spi_csn_high(&h);
-    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, ret);
+    TEST_ASSERT_EQUAL(LT_FAIL, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_spi_csn_high() returns LT_OK when lt_port_spi_csn_high() function returns LT_OK
-void test_lt_l1_spi_csn_high___fail_during_lt_port_spi_csn_high__LT_OK()
+void test_lt_l1_spi_csn_high___lt_port_spi_csn_high__LT_OK()
 {
     lt_handle_t h = {0};
 
@@ -134,21 +104,13 @@ void test_lt_l1_spi_csn_high___fail_during_lt_port_spi_csn_high__LT_OK()
     TEST_ASSERT_EQUAL(LT_OK, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-// Check that function lt_l1_spi_transfer() returns LT_L1_SPI_ERROR when lt_port_spi_transfer() function returns LT_L1_SPI_ERROR
-void test_lt_l1_spi_transfer___fail_during_lt_port_spi_transfer__LT_L1_SPI_ERROR()
-{
-    lt_handle_t h = {0};
 
-    lt_port_spi_transfer_ExpectAndReturn(&h, 1, 1, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
 
-    lt_ret_t ret = lt_l1_spi_transfer(&h, 1, 1, LT_L1_TIMEOUT_MS_DEFAULT);
-    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, ret);
-}
+
 
 //---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_spi_transfer() returns LT_L1_DATA_LEN_ERROR when lt_port_spi_transfer() function returns LT_L1_DATA_LEN_ERROR
-void test_lt_l1_spi_transfer___fail_during_lt_port_spi_transfer__LT_L1_DATA_LEN_ERROR()
+void test_lt_l1_spi_transfer___lt_port_spi_transfer__LT_L1_DATA_LEN_ERROR()
 {
     lt_handle_t h = {0};
 
@@ -158,9 +120,19 @@ void test_lt_l1_spi_transfer___fail_during_lt_port_spi_transfer__LT_L1_DATA_LEN_
     TEST_ASSERT_EQUAL(LT_L1_DATA_LEN_ERROR, ret);
 }
 
-//---------------------------------------------------------------------------------------------------------------------
+// Check that function lt_l1_spi_transfer() returns LT_FAIL when lt_port_spi_transfer() function returns LT_FAIL
+void test_lt_l1_spi_transfer___lt_port_spi_transfer__LT_FAIL()
+{
+    lt_handle_t h = {0};
+
+    lt_port_spi_transfer_ExpectAndReturn(&h, 1, 1, LT_L1_TIMEOUT_MS_DEFAULT, LT_FAIL);
+
+    lt_ret_t ret = lt_l1_spi_transfer(&h, 1, 1, LT_L1_TIMEOUT_MS_DEFAULT);
+    TEST_ASSERT_EQUAL(LT_FAIL, ret);
+}
+
 // Check that function lt_l1_spi_transfer() returns LT_OK when lt_port_spi_transfer() function returns LT_OK
-void test_lt_l1_spi_transfer___fail_during_lt_port_spi_transfer__LT_OK()
+void test_lt_l1_spi_transfer___lt_port_spi_transfer__LT_OK()
 {
     lt_handle_t h = {0};
 
@@ -172,7 +144,7 @@ void test_lt_l1_spi_transfer___fail_during_lt_port_spi_transfer__LT_OK()
 
 //---------------------------------------------------------------------------------------------------------------------
 // Check that function lt_l1_delay() returns LT_OK when lt_port_delay() function returns LT_OK
-void test_lt_l1_delay___fail_during_lt_port_delay__LT_OK()
+void test_lt_l1_delay___lt_port_delay__LT_OK()
 {
     lt_handle_t h = {0};
 
