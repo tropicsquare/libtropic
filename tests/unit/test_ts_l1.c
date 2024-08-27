1f2f7a183f6e9113b0636c5f6561d9f9948b7f83
Jan Medek <jan.medek@tropicsquare.com>
2024-08-27 10:47:11 +0200

    test the rest of lt_l1_read()

--- tests/unit/test_ts_l1.c
+++ tests/unit/test_ts_l1.c
@@ -15,6 +15,7 @@
 
 #define SOME_UNUSED_DEFAULT_BYTE 0xfe
 
+#define L2_BUFF_WHEN_NOT_LT_L1_DATA_LEN_ERROR 0
 
 void setUp(void)
 {
@@ -124,5 +125,99 @@ void test_lt_l1_read___CHIP_MODE_READY_LT_L1_SPI_ERROR()
     TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l1_read(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT));
 }
 
+// Used to force lt_handle_t.l2_buff[0] to contain CHIP_MODE_READY bit
+static lt_ret_t callback_CHIP_MODE_READY_fpga_no_resp(lt_handle_t* h, uint8_t offset, uint16_t tx_len,
+                                                      uint32_t timeout, int cmock_num_calls)
+{
+    if(cmock_num_calls % 2 == 0) {
+        h->l2_buff[0] = CHIP_MODE_READY_bit;
+        return LT_OK;
+    }
+    if(cmock_num_calls % 2 == 1) {
+        h->l2_buff[1] = 0xff;
+        return LT_OK;
+    }
+
+    // Just in case, return some invalid number:
+    return 0xfefe;
+}
+
+// Test if function returns LT_L1_CHIP_BUSY when chip status is ready and lt_handle_t.l2_buff[1] == 0xff
+void test_lt_l1_read___CHIP_MODE_READY_fpga_no_resp()
+{
+    lt_handle_t h = {0};
+
+    for(int i=0; i< LT_L1_READ_MAX_TRIES; i++) {
+        lt_l1_spi_csn_low_ExpectAndReturn(&h, LT_OK);
+        lt_l1_spi_transfer_StubWithCallback(callback_CHIP_MODE_READY_fpga_no_resp);
+        lt_l1_spi_csn_high_ExpectAndReturn(&h, LT_OK);
+        lt_l1_delay_ExpectAndReturn(&h, LT_L1_READ_RETRY_DELAY, LT_OK);
+    }
+    TEST_ASSERT_EQUAL(LT_L1_CHIP_BUSY, lt_l1_read(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT));
+}
+
+// Used to force lt_handle_t.l2_buff[2] to LT_L1_LEN_MAX
+static lt_ret_t callback_CHIP_MODE_READY_LT_L1_DATA_LEN_ERROR(lt_handle_t* h, uint8_t offset,
+                                                              uint16_t tx_len, uint32_t timeout,
+                                                              int cmock_num_calls)
+{
+    if(cmock_num_calls == 0) {
+        h->l2_buff[0] = CHIP_MODE_READY_bit;
+        return LT_OK;
+    }
+    if(cmock_num_calls == 1) {
+        h->l2_buff[1] = 0;
+        h->l2_buff[2] = LT_L1_LEN_MAX - 2;
+        return LT_OK;
+    }
+
+    // Just in case, return some invalid number:
+    return 0xfefe;
+}
+
+// Test if function returns LT_L1_DATA_LEN_ERROR if chip is in ready mode
+void test_lt_l1_read___CHIP_MODE_READY_LT_L1_DATA_LEN_ERROR()
+{
+    lt_handle_t h = {0};
+
+    lt_l1_spi_csn_low_ExpectAndReturn(&h, LT_OK);
+    lt_l1_spi_transfer_StubWithCallback(callback_CHIP_MODE_READY_LT_L1_DATA_LEN_ERROR);
+    lt_l1_spi_csn_high_ExpectAndReturn(&h, LT_OK);
+
+    TEST_ASSERT_EQUAL(LT_L1_DATA_LEN_ERROR, lt_l1_read(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT));
+}
+
+// Used to force lt_handle_t.l2_buff[2] to LT_L1_LEN_MAX
+static lt_ret_t callback_CHIP_MODE_READY_LT_L1_SPI_ERROR_2(lt_handle_t* h, uint8_t offset, uint16_t tx_len,
+                                                           uint32_t timeout, int cmock_num_calls)
+{
+    if(cmock_num_calls == 0) {
+        h->l2_buff[0] = CHIP_MODE_READY_bit;
+        return LT_OK;
+    }
+    if(cmock_num_calls == 1) {
+        h->l2_buff[1] = 0;
+        h->l2_buff[2] = L2_BUFF_WHEN_NOT_LT_L1_DATA_LEN_ERROR;
+        return LT_OK;
+    }
+    if(cmock_num_calls == 2) {
+        return LT_FAIL;
+    }
+
+    // Just in case, return some invalid number:
+    return 0xfefe;
+}
+
+// Test if function returns LT_L1_SPI_ERROR when receiving the rest of incoming bytes
+void test_lt_l1_read___CHIP_MODE_READY_LT_L1_SPI_ERROR_2()
+{
+    lt_handle_t h = {0};
+
+    lt_l1_spi_csn_low_ExpectAndReturn(&h, LT_OK);
+    lt_l1_spi_transfer_StubWithCallback(callback_CHIP_MODE_READY_LT_L1_SPI_ERROR_2);
+    lt_l1_spi_csn_high_ExpectAndReturn(&h, LT_OK);
+
+    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l1_read(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT));
+}
 
 //---------------------------------------------------------------------------------------------------------------------
