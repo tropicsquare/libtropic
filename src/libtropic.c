23deb9cc75a2ca7923dc8f904e0ccbf0d853e911
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2024-08-29 15:38:19 +0200

    Added ECC slot checks to libtropic

--- src/libtropic.c
+++ src/libtropic.c
@@ -238,7 +238,11 @@ lt_ret_t lt_random_get(lt_handle_t *h, uint8_t *buff, const uint16_t len)
 
 lt_ret_t lt_ecc_key_generate(lt_handle_t *h, const ecc_slot_t slot, const ecc_curve_type_t curve)
 {
-    if(slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX || !h || ((curve != CURVE_P256) && (curve != CURVE_ED25519))  ){
+    if(    !h
+        || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX
+        || slot < LT_L3_ECC_KEY_GENERATE_SLOT_MIN
+        || ((curve != CURVE_P256) && (curve != CURVE_ED25519))
+    ){
         return LT_PARAM_ERR;
     }
     if(h->session != SESSION_ON) {
@@ -271,7 +275,13 @@ lt_ret_t lt_ecc_key_generate(lt_handle_t *h, const ecc_slot_t slot, const ecc_cu
 
 lt_ret_t lt_ecc_key_read(lt_handle_t *h, const ecc_slot_t slot, uint8_t *key, const uint8_t keylen, ecc_curve_type_t *curve, ecc_key_origin_t *origin)
 {
-    if(!h || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX || !key || !curve || !origin) {
+    if(    !h
+        || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX
+        || slot < LT_L3_ECC_KEY_GENERATE_SLOT_MIN
+        || !key
+        || !curve
+        || !origin
+    ) {
         return LT_PARAM_ERR;
     }
     if(h->session != SESSION_ON) {
@@ -313,7 +323,14 @@ lt_ret_t lt_ecc_key_read(lt_handle_t *h, const ecc_slot_t slot, uint8_t *key, co
 
 lt_ret_t lt_ecc_eddsa_sign(lt_handle_t *h, const ecc_slot_t slot, const uint8_t *msg, const uint16_t msg_len, uint8_t *rs, const uint8_t rs_len)
 {
-    if(!h || !msg || !rs || rs_len < 64 || ((msg_len < LT_L3_EDDSA_SIGN_MSG_LEN_MIN) | (msg_len > LT_L3_EDDSA_SIGN_MSG_LEN_MAX))) {
+    if(    !h
+        || !msg 
+        || !rs
+        || rs_len < 64
+        || ((msg_len < LT_L3_EDDSA_SIGN_MSG_LEN_MIN) | (msg_len > LT_L3_EDDSA_SIGN_MSG_LEN_MAX))
+        || slot < LT_L3_ECC_KEY_GENERATE_SLOT_MIN        
+        || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX
+    ) {
         return LT_PARAM_ERR;
     }
     if(h->session != SESSION_ON) {
@@ -344,7 +361,13 @@ lt_ret_t lt_ecc_eddsa_sign(lt_handle_t *h, const ecc_slot_t slot, const uint8_t 
 
 lt_ret_t lt_ecc_ecdsa_sign(lt_handle_t *h, const ecc_slot_t slot, const uint8_t *msg, const uint16_t msg_len, uint8_t *rs, const uint16_t rs_len)
 {
-    if(!h || !msg || !rs || (msg_len > LT_L3_ECDSA_SIGN_MSG_LEN_MAX) || (rs_len < 64)) {
+    if(    !h 
+        || !msg 
+        || !rs
+        || (msg_len > LT_L3_ECDSA_SIGN_MSG_LEN_MAX) || (rs_len < 64)
+        || slot < LT_L3_ECC_KEY_GENERATE_SLOT_MIN        
+        || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX
+    ) {
         return LT_PARAM_ERR;
     }
     if(h->session != SESSION_ON) {
@@ -397,7 +420,7 @@ lt_ret_t lt_ecc_eddsa_sig_verify(const uint8_t *msg, const uint16_t msg_len, con
 
 lt_ret_t lt_ecc_key_erase(lt_handle_t *h, const ecc_slot_t slot)
 {
-    if(!h || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX) {
+    if(!h || slot < LT_L3_ECC_KEY_GENERATE_SLOT_MIN || slot > LT_L3_ECC_KEY_GENERATE_SLOT_MAX) {
         return LT_PARAM_ERR;
     }
     if(h->session != SESSION_ON) {
