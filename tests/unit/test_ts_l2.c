f400721eb811af105aa6c9710ba5f21e65d17ee0
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2024-08-30 15:38:12 +0200

    Added remaining L2 tests

--- tests/unit/test_ts_l2.c
+++ tests/unit/test_ts_l2.c
@@ -3,9 +3,10 @@
 
 #include "libtropic_common.h"
 #include "lt_l2.h"
-#include "lt_l2_frame_check.h"
+#include "lt_l2_api_structs.h"
 
 #include "mock_lt_l1.h"
+#include "mock_lt_l2_frame_check.h"
 #include "mock_lt_crc16.h"
 
 
@@ -21,116 +22,11 @@ void tearDown(void)
 {
 }
 
-static uint8_t test_data[] = {0x00,
-                           0x00,
-                           0x80,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                           0x2e,0x4e};
-
-
-// Check function's return value on correct frame
-void test_lt_l2_frame_check___CHIP_STATUS_READY__LT_OK()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_RESULT_OK;
-    crc16_IgnoreAndReturn(0x2e4e);
-    TEST_ASSERT_EQUAL(LT_OK, lt_l2_frame_check(test_data));
-}
-
-/* Check what function returns when frame looks valid (from first two bytes), but frame's CRC check fails.
-This may happen when payload is altered during transmission */
-void test_lt_l2_frame_check___CHIP_STATUS_READY__LT_CRC_ERR()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_REQUEST_OK;
-    crc16_IgnoreAndReturn(0xdead);
-    TEST_ASSERT_EQUAL(LT_L2_IN_CRC_ERR, lt_l2_frame_check(test_data));
-}
-
-// Test various other return values
-void test_lt_l2_frame_check___LT_L2_REQ_CONT()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_REQUEST_CONT;
-    TEST_ASSERT_EQUAL(LT_L2_REQ_CONT, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_RES_CONT()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_RESULT_CONT;
-    TEST_ASSERT_EQUAL(LT_L2_RES_CONT, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_HSK_ERR()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_HSK_ERR;
-    TEST_ASSERT_EQUAL(LT_L2_HSK_ERR, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_NO_SESSION()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_NO_SESSION;
-    TEST_ASSERT_EQUAL(LT_L2_NO_SESSION, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_TAG_ERR()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_TAG_ERR;
-    TEST_ASSERT_EQUAL(LT_L2_TAG_ERR, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_CRC_ERR()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_CRC_ERR;
-    TEST_ASSERT_EQUAL(LT_L2_CRC_ERR, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_GEN_ERR()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_GEN_ERR;
-    TEST_ASSERT_EQUAL(LT_L2_GEN_ERR, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_NO_RESP()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_NO_RESP;
-    TEST_ASSERT_EQUAL(LT_L2_NO_RESP, lt_l2_frame_check(test_data));
-}
-
-void test_lt_l2_frame_check___LT_L2_UNKNOWN_REQ()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = L2_STATUS_UNKNOWN_ERR;
-    TEST_ASSERT_EQUAL(LT_L2_UNKNOWN_REQ, lt_l2_frame_check(test_data));
-}
-
-// Test default behaviour when second byte is not recognized by parser
-void test_lt_l2_frame_check___LT_FAIL()
-{
-    test_data[0] = CHIP_MODE_READY_bit;
-    test_data[1] = INVALID_BYTE;
-    TEST_ASSERT_EQUAL(LT_L2_STATUS_NOT_RECOGNIZED, lt_l2_frame_check(test_data));
-}
-
 void test_lt_l2_transfer__fail_during_l1_write()
 {
     lt_handle_t h = {0};
     
-    add_crc_Ignore();
+    add_crc_Expect(h.l2_buff);
     lt_l1_write_ExpectAndReturn(&h, 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
     TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_transfer(&h));
 }
@@ -139,27 +35,35 @@ void test_lt_l2_transfer__fail_during_l1_read()
 {
     lt_handle_t h = {0};
 
-    add_crc_Ignore();
+    add_crc_Expect(h.l2_buff);
     lt_l1_write_ExpectAndReturn(&h, 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
     lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
     TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_transfer(&h));
 }
 
-// TODO: move lt_l2_frame_check to another compilation unit?
-// void test_lt_l2_transfer__fail_during_frame_check()
-// {
-//     lt_handle_t h = {0};
-//     // This is to force lt_l2_frame_check to return LT_OK.
-//     // Reason: functions from the same compilation unit are unmockable.
-//     // TODO this won't work
-//     h.l2_buff[1] = L2_STATUS_REQUEST_OK;
+void test_lt_l2_transfer__fail_during_frame_check()
+{
+    lt_handle_t h = {0};
 
-//     add_crc_Ignore();
-//     lt_l1_write_ExpectAndReturn(&h, L2_STATUS_REQUEST_OK + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
-//     lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_L2_IN_CRC_ERR);
 
-//     TEST_ASSERT_EQUAL(LT_OK, lt_l2_transfer(&h));
-// }
+    TEST_ASSERT_EQUAL(LT_L2_IN_CRC_ERR, lt_l2_transfer(&h));
+}
+
+void test_lt_l2_transfer__correct()
+{
+    lt_handle_t h = {0};
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+
+    TEST_ASSERT_EQUAL(LT_OK, lt_l2_transfer(&h));
+}
 
 //--------------------------------------------------------------------------------------------------
 
@@ -168,7 +72,7 @@ void test_lt_l2_encrypted_cmd__l1_write_fail()
     lt_handle_t h = {0};
     int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
 
-    add_crc_Ignore();
+    add_crc_Expect(h.l2_buff);
     lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
 
     TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_encrypted_cmd(&h));
@@ -179,11 +83,132 @@ void test_lt_l2_encrypted_cmd__l1_read_fail()
     lt_handle_t h = {0};
     int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
 
-    add_crc_Ignore();
+    add_crc_Expect(h.l2_buff);
     lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
     lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
 
     TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_encrypted_cmd(&h));
 }
 
-// TODO: frame check
+void test_lt_l2_encrypted_cmd__frame_check_fail()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_L2_IN_CRC_ERR);
+
+    TEST_ASSERT_EQUAL(LT_L2_IN_CRC_ERR, lt_l2_encrypted_cmd(&h));
+}
+
+// Test two chunk long packet, throw an error on the second chunk.
+void test_lt_l2_encrypted_cmd__multiple_chunks()
+{
+    lt_handle_t h = {0};
+
+    struct lt_l3_gen_frame_t * p_frame = (struct lt_l3_gen_frame_t*)h.l3_buff;
+    p_frame->packet_size = L2_CHUNK_MAX_DATA_SIZE * 2;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, L2_CHUNK_MAX_DATA_SIZE + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_L2_REQ_CONT);
+    
+    lt_l1_write_ExpectAndReturn(&h, L2_CHUNK_MAX_DATA_SIZE + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
+
+    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_encrypted_cmd(&h));
+}
+
+void test_lt_l2_encrypted_cmd__recv_lt_l1_read_fail()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_L1_SPI_ERROR);
+
+    TEST_ASSERT_EQUAL(LT_L1_SPI_ERROR, lt_l2_encrypted_cmd(&h)); 
+}
+
+lt_ret_t test_lt_l2_encrypted_cmd__overflow_callback(const uint8_t *frame, int cmock_num_calls)
+{
+    if (cmock_num_calls > 0) {
+        struct lt_l2_encrypted_cmd_rsp_t* resp = (struct lt_l2_encrypted_cmd_rsp_t*) frame;
+        resp->rsp_len = 255;
+        return LT_L2_RES_CONT;
+    } else {
+        return LT_OK;
+    }
+
+}
+
+// This is kinda tricky. resp->rsp_len is unsigned 8bit int => max value cannot be larger
+// than L3_FRAME_MAX_SIZE, so overflow cannot be triggered by a callback. So, we need to
+// trigger this by making offset larger.
+void test_lt_l2_encrypted_cmd__recv_overflow()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_Stub(test_lt_l2_encrypted_cmd__overflow_callback);
+    lt_l1_read_IgnoreAndReturn(LT_OK);
+
+    TEST_ASSERT_EQUAL(LT_L3_DATA_LEN_ERROR, lt_l2_encrypted_cmd(&h)); 
+}
+
+void test_lt_l2_encrypted_cmd__recv_last_frame()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+
+    TEST_ASSERT_EQUAL(LT_OK, lt_l2_encrypted_cmd(&h));   
+}
+
+void test_lt_l2_encrypted_cmd__recv_frame_err()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_L2_IN_CRC_ERR);
+
+    TEST_ASSERT_EQUAL(LT_L2_IN_CRC_ERR, lt_l2_encrypted_cmd(&h));   
+}
+
+void test_lt_l2_encrypted_cmd__loop_overflow()
+{
+    lt_handle_t h = {0};
+    int chunk_len = L3_RES_SIZE_SIZE + L3_TAG_SIZE;
+
+    add_crc_Expect(h.l2_buff);
+    lt_l1_write_ExpectAndReturn(&h, chunk_len + 4, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+    lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_OK);
+
+    for (int i = 0; i < 42; i++) {
+        lt_l1_read_ExpectAndReturn(&h, LT_L1_LEN_MAX, LT_L1_TIMEOUT_MS_DEFAULT, LT_OK);
+        lt_l2_frame_check_ExpectAndReturn(h.l2_buff, LT_L2_RES_CONT);
+    }
+
+    TEST_ASSERT_EQUAL(LT_FAIL, lt_l2_encrypted_cmd(&h));   
+}
+
