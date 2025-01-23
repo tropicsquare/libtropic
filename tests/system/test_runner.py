be9c0dfb113dcf06ecc9ea49b9d30961e36bc07c
Ondrej Golasowski <golasowski.o@gmail.com>
2025-01-23 14:20:00 +0100

    feat(system test runner): added adapter mapping config file support, added serial exception handling.

--- tests/system/test_runner.py
+++ tests/system/test_runner.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import logging
 import subprocess
 import shutil
@@ -122,6 +123,30 @@ def cleanup(openocd_proc: subprocess.Popen):
             openocd_proc.kill()
         logger.info("Done. Bye.")
 
+class lt_environment_tools:
+    @dataclass
+    class lt_adapter_id:
+        vid: int
+        pid: int
+    
+    @staticmethod
+    def get_adapter_id_from_mapping(platform_str: str, map_file_path: Path) -> lt_adapter_id | None:
+        if not map_file_path.exists() or not map_file_path.is_file():
+            logger.error("Config file not found!")
+            return None
+        with map_file_path.open() as f:
+            if f.readline().rstrip("\n") != "platform;vid;pid":
+                logger.error("Incorrect config file header!")
+                return None
+            while line := f.readline().rstrip("\n"):
+                if len(line_separated := line.split(';')) != 3:
+                    logger.error("Incorrect config file line format!")
+                    return None
+                if line_separated[0] == platform_str:
+                    return lt_environment_tools.lt_adapter_id(vid = line_separated[1], pid = line_separated[2])
+            logger.error("Platform not found in config file!")
+            return None
+
 async def main():
     print("libtropic SLT start")
     logging.basicConfig(level=logging.INFO)
@@ -133,10 +158,12 @@ async def main():
     work_dir = Path("tests/system/build")
     adapter_config_path = Path(__file__).parent / "ts11-jtag.cfg"
     platform = lt_platform_factory(platform_string_id)
+    adapter_mapping_path = Path("tests/system/adapter_mapping.csv") 
+    adapter_id = lt_environment_tools.get_adapter_id_from_mapping(platform_string_id, adapter_mapping_path)
 
     try:
         logger.info("Launching OpenOCD...")
-        openocd_launch_params = ["-f", adapter_config_path] + platform.get_openocd_launch_params()
+        openocd_launch_params = ["-f", adapter_config_path] + ["-c", f"ftdi vid_pid {adapter_id.vid} {adapter_id.pid}"] + platform.get_openocd_launch_params() 
         openocd_proc = subprocess.Popen([shutil.which("openocd")] + openocd_launch_params, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         await asyncio.sleep(2)
@@ -147,8 +174,12 @@ async def main():
 
         tr = lt_test_runner(work_dir, platform, "/dev/ttyUSB1")
         await tr.run(firmware_path)
+    except serial.SerialException as e:
+        logger.error(f"Platform serial interface communication error: {str(e)}")
+        cleanup(openocd_proc)
+        return
     except:
-        logger.info("Received exception or termination request. Shutting down.")
+        logger.info("Received unexpected exception or termination request. Shutting down.")
         cleanup(openocd_proc)
         raise
 
