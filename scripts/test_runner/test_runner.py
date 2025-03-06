2aea322ea646c4d73ad9904d15d88ea57968bd16
Ondrej Golasowski <golasowski.o@gmail.com>
2025-03-06 14:20:18 +0100

    feat(test runner): added openocd function for led, init, spi and power.

--- scripts/test_runner/test_runner.py
+++ scripts/test_runner/test_runner.py
@@ -8,7 +8,7 @@ import serial.tools
 import serial.tools.list_ports
 import telnetlib3
 import serial
-from enum import Enum
+from enum import Enum, IntEnum
 from abc import ABC, abstractmethod
 from pathlib import Path
 from argparse import ArgumentParser
@@ -16,6 +16,16 @@ from argparse import ArgumentParser
 logger = logging.getLogger(__name__)
 
 class lt_platform(ABC):
+    class lt_led_color(IntEnum):
+        BLACK = 0x0
+        RED = 0x1,
+        GREEN = 0x2,
+        BLUE = 0x4,
+        YELLOW = 0x3,
+        CYAN = 0x6,
+        MAGENTA = 0x5,
+        WHITE = 0x7
+
     def __init__(self):
         pass
     
@@ -26,6 +36,9 @@ class lt_platform(ABC):
         logger.info("Opening connection to OpenOCD...")
         self.ocd_reader, self.ocd_writer = await telnetlib3.open_connection('localhost', 4444)
 
+        await self.ocd_reader.readuntil(">".encode('ascii'))
+        logger.info("OpenOCD ready!")
+
     def openocd_disconnect(self):
         logger.info("Disconnecting from OpenOCD...")
         self.ocd_reader.close()
@@ -34,13 +47,55 @@ class lt_platform(ABC):
     async def openocd_send(self, cmd: str, timeout: float = 5):
         self.ocd_writer.write(cmd)
         await asyncio.wait_for(self.ocd_writer.drain(), timeout)
+    
+    async def openocd_recv(self, timeout: float = 5) -> str:
+        ocd_line = await asyncio.wait_for(self.ocd_reader.readline(), timeout)
+        return ocd_line
 
-    async def openocd_recv_match(self, match_str: str, timeout: float = 5):
+    async def openocd_recv_match(self, match_str: str, timeout: float = 5) -> str:
         ocd_line = ""
         while not match_str in ocd_line:
             ocd_line = await asyncio.wait_for(self.ocd_reader.readline(), timeout)
-            logger.info(f"OpenOCD:{ocd_line[:-1]}")
+        return ocd_line
+    
+    ####################################################
+    # Common OpenOCD functions
+    ####################################################
+    async def set_disco_led(self, color: lt_led_color):
+        # Get state of each LED for requested color and convert to 0 (off) or 1 (on).
+        # This int(bool()) stuff is just to get 0 or 1 from the masked value (instead of shifting).
+        await self.openocd_send(f"ftdi set_signal LED_R {int(bool(color.value & 0x1))}\n")
+        await self.openocd_send(f"ftdi set_signal LED_G {int(bool(color.value & 0x2))}\n")
+        await self.openocd_send(f"ftdi set_signal LED_B {int(bool(color.value & 0x4))}\n")
+        # Wait for the values to propagate.
+        await asyncio.sleep(0.001)
 
+    async def blink_disco_led(self, color: lt_led_color, count = 3):
+        for _ in range(count):
+            await self.set_disco_led(color)
+            await asyncio.sleep(0.1)
+            await self.set_disco_led(self.lt_led_color.BLACK)
+            await asyncio.sleep(0.1)        
+
+    async def initialize(self):
+        await self.openocd_send("init\n")
+
+    async def set_spi_en(self, state: bool):
+        await self.openocd_send(f"ftdi set_signal SPI_EN {int(state)}\n")
+
+    async def is_spi_bus_free(self) -> bool:
+        await self.openocd_send(f"ftdi get_signal SPI_EN_BUS\n")
+        openocd_response = await self.openocd_recv_match("Signal SPI_EN_BUS =", 30)
+        return ("Signal SPI_EN_BUS = 000000" in openocd_response)
+
+    async def set_platform_power(self, state: bool):
+        await self.openocd_send(f"ftdi set_signal PLTF_PWR_EN {int(state)}\n")
+    ####################################################
+    # Platform-specific OpenOCD functions
+    # 
+    # These functions shall be specified in
+    # child classes, as they differ for each platform.
+    ####################################################
     @abstractmethod
     def get_openocd_launch_params(self):
         pass
@@ -112,19 +167,38 @@ class lt_test_runner:
     async def run(self, elf_path: Path) -> lt_test_result:
         await self.platform.openocd_connect()
 
+        await self.platform.initialize()
+        await self.platform.set_platform_power(True)
+
+        if not await self.platform.is_spi_bus_free():
+            logger.error("SPI bus is already occupied! Check if all other platform boards disabled SPI access!")
+            self.platform.openocd_disconnect()
+            return self.lt_test_result.TEST_FAILED
+
+        await self.platform.set_spi_en(True)
+
+        await self.platform.blink_disco_led(lt_platform.lt_led_color.WHITE)
+        await self.platform.set_disco_led(lt_platform.lt_led_color.WHITE)
+
         try:
             await self.platform.load_elf(elf_path)
         except TimeoutError:
             logger.error("Communication with OpenOCD timed out while loading firmware!")
+            self.platform.openocd_disconnect()
+            return self.lt_test_result.TEST_FAILED
 
         err_count = 0
         warn_count = 0
         assert_fail_count = 0
         try:
-            with serial.Serial(self.serial_port, baudrate = 115200, timeout=10, inter_byte_timeout=10) as s:
+            with serial.Serial(self.serial_port, baudrate = 115200, timeout=10, inter_byte_timeout=10, exclusive=True) as s:
                 await self.platform.reset()
             
                 while True:
+                    if not s.is_open:
+                        logger.error("Serial port closed! Check if it is not used by other application (screen, GTKTerm...).")
+                        break
+
                     line = s.read_until(expected=b"\r\n").decode('ascii', errors="backslashreplace")
                     logger.debug(f"Received from serial: {line}")
                     line = line.split(";")
@@ -156,8 +230,6 @@ class lt_test_runner:
         except TimeoutError:
             logger.error("Serial timeout! Did not receive any message in time. Check if the test output is correct and if TEST_FINISH is issued at the end of the test.")
 
-        self.platform.openocd_disconnect()
-
         logger.info("------ Stats ------")
         logger.info(f"Errors: {err_count}")
         logger.info(f"Warnings: {warn_count}")
@@ -165,8 +237,12 @@ class lt_test_runner:
 
         if err_count > 0 or warn_count > 0 or assert_fail_count > 0:
             logger.error("There were errors or warnings, test unsuccessful!")
+            await self.platform.set_disco_led(lt_platform.lt_led_color.RED)
+            self.platform.openocd_disconnect()
             return self.lt_test_result.TEST_FAILED
 
+        await self.platform.set_disco_led(lt_platform.lt_led_color.GREEN)
+        self.platform.openocd_disconnect()
         return self.lt_test_result.TEST_PASSED
 
 def cleanup(openocd_proc: subprocess.Popen):
