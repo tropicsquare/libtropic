cfb31927be745fe3bffe12aee86ae8b6424cf804
Pavel Polach <pavel.polach@tropicsquare.com>
2025-04-02 11:12:52 +0200

    feat (test_runner) : openocd F439 script now accepts path to elf and test duration in sec

--- scripts/test_runner/openocd_test_F439ZI.sh
+++ scripts/test_runner/openocd_test_F439ZI.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-# Update amount of sleep to match the time needed for the test to run. Find it on line after reset
+##########################################################################################################
+# Usage:
+# [1] Pass path to the ELF file which will be flashed, for example:
+# ./openocd_test_F439ZI.sh "/home/xxx/Desktop/libtropic-stm32/NUCLEO_F439ZI/build/stm32_example.elf"
+# [2] Update amount of sleep to match the time needed for the test to run
 
-# Update path to the ELF file
-PATH_TO_STM32_ELF="/home/ppolach/Desktop/libtropic-stm32/NUCLEO_F439ZI/build/stm32_example.elf"
-# Openocd uses full paths, so we need to get the path to this folder
+##########################################################################################################
+
+PATH_TO_STM32_ELF="$1"
+TEST_EXECUTION_IN_SECONDS="$2"
+
 PATH_TO_THIS_FOLDER=`pwd`
+
+# Check if the ELF file path is provided
+if [ -z "$PATH_TO_STM32_ELF" ]; then
+  echo "Error: No ELF file path provided."
+  echo "Usage: $0 <path_to_elf_file> <test_execution_in_seconds>"
+  exit 1
+fi
+
+# Check if the test execution time is provided
+if [ -z "$TEST_EXECUTION_IN_SECONDS" ]; then
+  echo "Error: No test execution time provided."
+  echo "Usage: $0 <path_to_elf_file> <test_execution_in_seconds>"
+  exit 1
+fi
 
 echo ""
 echo ""
@@ -32,7 +52,7 @@ echo ""
 echo ""
 openocd -f ${PATH_TO_THIS_FOLDER}/ts11-jtag.cfg -f /usr/share/openocd/scripts/target/stm32f4x.cfg -c "init; reset; exit"
 # Amount of sleep == time needed for test to run
-sleep 5
+sleep ${TEST_EXECUTION_IN_SECONDS}
 
 echo ""
 echo ""
