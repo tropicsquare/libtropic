7edf6906cef16cd3577f4bc4b233f37d44a813fb
Ondrej Golasowski <ondrej.golasowski@tropicsquare.com>
2025-06-27 08:21:11 +0200

    feat: added static assert macro.

---
+++ include/tools.h
@@ -0,0 +1,9 @@
+/**
+ * @file tools.h
+ * @brief Various helper macros.
+ * @author Tropic Square s.r.o.
+ *
+ * @license For the license see file LICENSE.md file in the root directory of this source tree.
+ */
+
+#define STATIC_ASSERT(x) _Static_assert((x), "Static assertion failed");
