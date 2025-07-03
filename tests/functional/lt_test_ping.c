7eef1607d9537dba09b373c33fa16171874511d7
Jan Medek <czmedexs@gmail.com>
2025-07-03 15:06:21 +0200

    test(lt_test_ping): ping only slot 0

--- tests/functional/lt_test_ping.c
+++ tests/functional/lt_test_ping.c
@@ -1,6 +1,6 @@
 /**
  * @file lt_test_ping.c
- * @brief Test function which tries Ping command with max length of message with all 4 pairing slots
+ * @brief Test function which tries Ping command with max length of message with pairing slot 0
  * @author Tropic Square s.r.o.
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
@@ -53,19 +53,6 @@ int lt_test_ping(void)
     LT_LOG("%s", "Initialize handle");
     LT_ASSERT(LT_OK, lt_init(&h));
 
-    // Start secure session with slot 0
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_0);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
-
-    // Write pairing keys into slot 1, 2 and 3
-    LT_LOG("%s", "Writing pairing key H1");
-    LT_ASSERT(LT_OK, lt_pairing_key_write(&h, sh1pub, PAIRING_KEY_SLOT_INDEX_1));
-    LT_LOG("%s", "Writing pairing key H2");
-    LT_ASSERT(LT_OK, lt_pairing_key_write(&h, sh2pub, PAIRING_KEY_SLOT_INDEX_2));
-    LT_LOG("%s", "Writing pairing key H3");
-    LT_ASSERT(LT_OK, lt_pairing_key_write(&h, sh3pub, PAIRING_KEY_SLOT_INDEX_3));
-    LT_LOG_LINE();
-
     // Ping with SH0
     LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_0);
     LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
@@ -77,41 +64,6 @@ int lt_test_ping(void)
     LT_LOG("%s", "lt_session_abort()");
     LT_ASSERT(LT_OK, lt_session_abort(&h));
     memset(in, 0x00, PING_LEN);
-
-    // Ping with SH1
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_1);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh1priv, sh1pub, PAIRING_KEY_SLOT_INDEX_1));
-    LT_LOG("%s", "lt_ping() ");
-    LT_ASSERT(LT_OK, lt_ping(&h, ping_msg, in, PING_LEN));
-    LT_LOG("Asserting %d B of Ping message", PING_LEN);
-    LT_ASSERT(0, memcmp(in, ping_msg, PING_LEN));
-    LT_LOG_LINE();
-    LT_LOG("%s", "lt_session_abort()");
-    LT_ASSERT(LT_OK, lt_session_abort(&h));
-    memset(in, 0x00, PING_LEN);
-
-    // Ping with SH2
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_2);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh2priv, sh2pub, PAIRING_KEY_SLOT_INDEX_2));
-    LT_LOG("%s", "lt_ping() ");
-    LT_ASSERT(LT_OK, lt_ping(&h, ping_msg, in, PING_LEN));
-    LT_LOG("Asserting %d B of Ping message", PING_LEN);
-    LT_ASSERT(0, memcmp(in, ping_msg, PING_LEN));
-    LT_LOG_LINE();
-    LT_LOG("%s", "lt_session_abort()");
-    LT_ASSERT(LT_OK, lt_session_abort(&h));
-    memset(in, 0x00, PING_LEN);
-
-    // Ping with SH3
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_3);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh3priv, sh3pub, PAIRING_KEY_SLOT_INDEX_3));
-    LT_LOG("%s", "lt_ping() ");
-    LT_ASSERT(LT_OK, lt_ping(&h, ping_msg, in, PING_LEN));
-    LT_LOG("Asserting %d B of Ping message", PING_LEN);
-    LT_ASSERT(0, memcmp(in, ping_msg, PING_LEN));
-    LT_LOG_LINE();
-    LT_LOG("%s", "lt_session_abort()");
-    LT_ASSERT(LT_OK, lt_session_abort(&h));
 
     // Deinit handle
     LT_LOG("%s", "lt_deinit()");
