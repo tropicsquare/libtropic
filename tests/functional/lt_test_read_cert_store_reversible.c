a157bb10b344c3210b8d0fe64db91ce417b76158
Jan Medek <czmedexs@gmail.com>
2025-07-09 16:13:39 +0200

    refactor(lt_test_read_cert_store_reversible): fix formatting

--- tests/functional/lt_test_read_cert_store_reversible.c
+++ tests/functional/lt_test_read_cert_store_reversible.c
@@ -14,7 +14,6 @@
 /** @brief Length of the buffers for certificates. */
 #define CERTS_BUF_LEN 700
 
-
 void lt_test_read_cert_store_reversible(void)
 {
     LT_LOG_INFO("----------------------------------------------");
@@ -28,8 +27,8 @@ void lt_test_read_cert_store_reversible(void)
     h.l3.buff_len = sizeof(l3_buffer);
 #endif
     uint8_t cert1[CERTS_BUF_LEN], cert2[CERTS_BUF_LEN], cert3[CERTS_BUF_LEN], cert4[CERTS_BUF_LEN];
-    struct lt_cert_store_t store = {.certs    = {cert1, cert2, cert3, cert4},
-                                    .buf_len  = {CERTS_BUF_LEN, CERTS_BUF_LEN, CERTS_BUF_LEN, CERTS_BUF_LEN}};
+    struct lt_cert_store_t store = {.certs = {cert1, cert2, cert3, cert4},
+                                    .buf_len = {CERTS_BUF_LEN, CERTS_BUF_LEN, CERTS_BUF_LEN, CERTS_BUF_LEN}};
 
     LT_LOG_INFO("Initializing handle");
     LT_ASSERT(LT_OK, lt_init(&h));
@@ -46,8 +45,9 @@ void lt_test_read_cert_store_reversible(void)
         cert = store.certs[i];
         for (int j = 0; j < store.cert_len[i]; j += 16)
             LT_LOG_INFO("%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", cert[j], cert[j + 1],
-                   cert[j + 2], cert[j + 3], cert[j + 4], cert[j + 5], cert[j + 6], cert[j + 7], cert[j + 8],
-                   cert[j + 9], cert[j + 10], cert[j + 11], cert[j + 12], cert[j + 13], cert[j + 14], cert[j + 15]);
+                        cert[j + 2], cert[j + 3], cert[j + 4], cert[j + 5], cert[j + 6], cert[j + 7], cert[j + 8],
+                        cert[j + 9], cert[j + 10], cert[j + 11], cert[j + 12], cert[j + 13], cert[j + 14],
+                        cert[j + 15]);
         LT_LOG_INFO();
     }
     LT_LOG_LINE();
