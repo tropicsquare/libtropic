35c54d198449a6482affadce89f045764d9cfd07
Jan Medek <czmedexs@gmail.com>
2025-07-14 17:22:02 +0200

    test(lt_test_r_mem_reversible): finish test, add doxygen comments

--- tests/functional/lt_test_r_mem_reversible.c
+++ tests/functional/lt_test_r_mem_reversible.c
@@ -14,17 +14,9 @@
 
 void lt_test_r_mem_reversible(void)
 {
-    LT_LOG(
-        "  "
-        "------------------------------------------------------------------------------------------------------------"
-        "-");
-    LT_LOG(
-        "  -------------- lt_test_r_mem_reversible()  "
-        "-----------------------------------------------------------------------------");
-    LT_LOG(
-        "  "
-        "------------------------------------------------------------------------------------------------------------"
-        "-");
+    LT_LOG_INFO("----------------------------------------------");
+    LT_LOG_INFO("lt_test_r_mem_reversible()");
+    LT_LOG_INFO("----------------------------------------------");
 
     lt_handle_t h = {0};
 #if LT_SEPARATE_L3_BUFF
@@ -32,56 +24,60 @@ void lt_test_r_mem_reversible(void)
     h.l3.buff = l3_buffer;
     h.l3.buff_len = sizeof(l3_buffer);
 #endif
-    uint8_t r_mem_data[R_MEM_DATA_SIZE_MAX];
-    uint8_t r_mem_data_expected[R_MEM_DATA_SIZE_MAX];
-    uint16_t read_data_size;
+    uint8_t r_mem_data1[R_MEM_DATA_SIZE_MAX], r_mem_data2[R_MEM_DATA_SIZE_MAX];
+    uint16_t read_data_size1, read_data_size2;
 
-    LT_LOG("%s", "Initialize handle");
+    LT_LOG_INFO("Initializing handle");
     LT_ASSERT(LT_OK, lt_init(&h));
 
-    // Start secure session with SH0
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_0);
+    LT_LOG_INFO("Starting Secure Session with key %d", PAIRING_KEY_SLOT_INDEX_0);
     LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
+    LT_LOG_LINE();
 
-    LT_LOG("---------------- Loop through all mem slots and check if they are empty ----------------");
-
-    LT_LOG("Looping through slots 0-%d.", R_MEM_DATA_SLOT_MAX);
+    LT_LOG_INFO("Looping through slots 0-%d and checking if empty...", R_MEM_DATA_SLOT_MAX);
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
-        LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+        LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data1, &read_data_size1));
+        LT_ASSERT(1, (read_data_size1 == 0));
     }
-    LT_LOG("All slots are empty.");
+    LT_LOG_INFO("All slots are empty");
+    LT_LOG_LINE();
 
-    // LT_LOG("---------------- Loop through all mem slots, write 0xAB and check if written ----------------");
+    LT_LOG_INFO("Looping through slots 0-%d, writing 0xAB and checking if written...", R_MEM_DATA_SLOT_MAX);
+    uint8_t write_data[R_MEM_DATA_SIZE_MAX];
+    memset(write_data, 0xAB, R_MEM_DATA_SIZE_MAX);
+    for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_ASSERT(LT_OK, lt_r_mem_data_write(&h, i, write_data, R_MEM_DATA_SIZE_MAX));
+        LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data1, &read_data_size1));
+        LT_ASSERT(1, (read_data_size1 == R_MEM_DATA_SIZE_MAX));
+        LT_ASSERT(0, memcmp(r_mem_data1, write_data, R_MEM_DATA_SIZE_MAX));
+    }
+    LT_LOG_INFO("All slots were successfuly written");
+    LT_LOG_LINE();
 
-    // uint8_t write_data[R_MEM_DATA_SIZE_MAX];
-    // memset(write_data, 0xAB, R_MEM_DATA_SIZE_MAX);
-    // LT_LOG("Looping through slots 0-%d.", R_MEM_DATA_SLOT_MAX);
-    // for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
-    //     LT_ASSERT(LT_OK, lt_r_mem_data_write(&h, i, write_data, R_MEM_DATA_SIZE_MAX));
-    //     LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, R_MEM_DATA_SIZE_MAX));
-    //     LT_ASSERT(0, memcmp(r_mem_data, write_data, R_MEM_DATA_SIZE_MAX));
-    // }
-    // LT_LOG("All slots were successfuly written.");
+    LT_LOG_INFO("Looping through slots 0-%d, writing 0x00 and checking for fail...", R_MEM_DATA_SLOT_MAX);
+    uint8_t zeros[R_MEM_DATA_SIZE_MAX];
+    memset(zeros, 0, R_MEM_DATA_SIZE_MAX);
+    for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_ASSERT(LT_L3_R_MEM_DATA_WRITE_WRITE_FAIL, lt_r_mem_data_write(&h, i, zeros, R_MEM_DATA_SIZE_MAX));
+        LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data2, &read_data_size2));
+        LT_ASSERT(1, (read_data_size2 == R_MEM_DATA_SIZE_MAX));
+        LT_ASSERT(0, memcmp(r_mem_data2, write_data, R_MEM_DATA_SIZE_MAX));
+    }
+    LT_LOG_INFO("No slots were written (correct)");
+    LT_LOG_LINE();
 
-    // LT_LOG("---------------- Loop through all mem slots and erase them ----------------");
+    LT_LOG_INFO("Loop through slots 0-%d, erasing them and checking if erased...", R_MEM_DATA_SLOT_MAX);
+    for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_ASSERT(LT_OK, lt_r_mem_data_erase(&h, i));
+        LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data1, &read_data_size1));
+        LT_ASSERT(1, (read_data_size1 == 0));
+    }
+    LT_LOG_INFO("All slots were successfuly erased");
+    LT_LOG_LINE();
 
-    // LT_LOG("Looping through slots 0-%d.", R_MEM_DATA_SLOT_MAX);
-    // for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
-    //     LT_ASSERT(LT_OK, lt_r_mem_data_erase(&h, i));
-    //     // TODO: Read should fail
-    //     LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, R_MEM_DATA_SIZE_MAX));
-
-    //     // LT_LOG("Slot #%d after erasing:", i);
-    //     // printf("r_mem_data[] = {");
-    //     // for (int j = 0; j < R_MEM_DATA_SIZE_MAX; j++)
-    //     //     printf("0x%02x, ", r_mem_data[j]);
-    //     // printf("};\n");
-    // }
-    // LT_LOG("All slots were successfuly erased.");
-
-    LT_LOG("%s", "lt_session_abort()");
+    LT_LOG_INFO("Aborting Secure Session");
     LT_ASSERT(LT_OK, lt_session_abort(&h));
 
-    LT_LOG("%s", "lt_deinit()");
+    LT_LOG_INFO("Deinitializing handle");
     LT_ASSERT(LT_OK, lt_deinit(&h));
 }
