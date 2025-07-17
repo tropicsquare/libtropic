19a75e96f53c559eb0a38eb200522baa00770e63
Jan Medek <czmedexs@gmail.com>
2025-07-17 14:28:19 +0200

    test(lt_test_rev_r_mem): add randomization, enhance logging

--- tests/functional/lt_test_rev_r_mem.c
+++ tests/functional/lt_test_rev_r_mem.c
@@ -10,6 +10,7 @@
 #include "libtropic_common.h"
 #include "libtropic_functional_tests.h"
 #include "libtropic_logging.h"
+#include "libtropic_port.h"
 #include "string.h"
 
 void lt_test_rev_r_mem(void)
@@ -26,6 +27,7 @@ void lt_test_rev_r_mem(void)
 #endif
     uint8_t r_mem_data[R_MEM_DATA_SIZE_MAX], write_data[R_MEM_DATA_SIZE_MAX], zeros[R_MEM_DATA_SIZE_MAX] = {0};
     uint16_t read_data_size;
+    uint32_t random_data[R_MEM_DATA_SIZE_MAX / sizeof(uint32_t)], random_data_size;
 
     LT_LOG_INFO("Initializing handle");
     LT_ASSERT(LT_OK, lt_init(&h));
@@ -34,68 +36,105 @@ void lt_test_rev_r_mem(void)
     LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
     LT_LOG_LINE();
 
-    LT_LOG_INFO("Looping through slots 0-%d and checking if empty...", R_MEM_DATA_SLOT_MAX);
+    LT_LOG_INFO("Checking if all slots are empty...");
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Reading slot #%d (should fail)...", i);
         LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+
+        LT_LOG_INFO("Checking number of read bytes (should be 0)...");
         LT_ASSERT(1, (read_data_size == 0));
     }
-    LT_LOG_INFO("All slots are empty");
     LT_LOG_LINE();
-
-    LT_LOG_INFO("Looping through slots 0-%d, writing them entirely with 0xAB and checking if written...",
-                R_MEM_DATA_SLOT_MAX);
-    memset(write_data, 0xAB, R_MEM_DATA_SIZE_MAX);
+    
+    LT_LOG_INFO("Testing writing all slots entirely...");
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Generating random data for slot #%d...", i);
+        LT_ASSERT(LT_OK, lt_port_random_bytes(random_data, sizeof(random_data) / sizeof(uint32_t)));
+        memcpy(write_data, random_data, sizeof(write_data));
+
+        LT_LOG_INFO("Writing to slot #%d...", i);
         LT_ASSERT(LT_OK, lt_r_mem_data_write(&h, i, write_data, R_MEM_DATA_SIZE_MAX));
-        LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
-        LT_ASSERT(1, (read_data_size == R_MEM_DATA_SIZE_MAX));
-        LT_ASSERT(0, memcmp(r_mem_data, write_data, R_MEM_DATA_SIZE_MAX));
-    }
-    LT_LOG_INFO("All slots were successfuly written");
-    LT_LOG_LINE();
 
-    LT_LOG_INFO("Looping through slots 0-%d, writing 0x00 and checking for fail...", R_MEM_DATA_SLOT_MAX);
-    // Set these two to different values just in case
-    read_data_size = 0;
-    memset(r_mem_data, 0, R_MEM_DATA_SIZE_MAX);
-    for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_LOG_INFO("Reading slot #%d...", i);
+        LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+
+        LT_LOG_INFO("Checking number of read bytes...");
+        LT_ASSERT(1, (read_data_size == R_MEM_DATA_SIZE_MAX));
+
+        LT_LOG_INFO("Checking contents...");
+        LT_ASSERT(0, memcmp(r_mem_data, write_data, R_MEM_DATA_SIZE_MAX));
+
+        LT_LOG_INFO("Writing zeros to slot #%d (should fail)...", i);
         LT_ASSERT(LT_L3_R_MEM_DATA_WRITE_WRITE_FAIL, lt_r_mem_data_write(&h, i, zeros, R_MEM_DATA_SIZE_MAX));
+
+        LT_LOG_INFO("Reading slot #%d...", i);
+        read_data_size = 0; // Set different value just in case
         LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+
+        LT_LOG_INFO("Checking number of read bytes...");
         LT_ASSERT(1, (read_data_size == R_MEM_DATA_SIZE_MAX));
+
+        LT_LOG_INFO("Checking contents (should still contain original data)...");
         LT_ASSERT(0, memcmp(r_mem_data, write_data, R_MEM_DATA_SIZE_MAX));
     }
-    LT_LOG_INFO("No slots were written (correct)");
     LT_LOG_LINE();
 
-    LT_LOG_INFO("Looping through slots 0-%d, erasing them and checking if erased...", R_MEM_DATA_SLOT_MAX);
+    LT_LOG_INFO("Erasing all slots...");
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Erasing slot #%d...", i);
         LT_ASSERT(LT_OK, lt_r_mem_data_erase(&h, i));
+
+        LT_LOG_INFO("Reading slot #%d (should fail)...", i);
         LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+
+        LT_LOG_INFO("Checking number of read bytes (should be 0)...");
         LT_ASSERT(1, (read_data_size == 0));
     }
-    LT_LOG_INFO("All slots were successfuly erased");
     LT_LOG_LINE();
 
-    LT_LOG_INFO("Looping through slots 0-%d, writing them partially with 0xCD and checking if written...",
-                R_MEM_DATA_SLOT_MAX);
-    memset(write_data, 0xCD, 100);
+    LT_LOG_INFO("Testing writing all slots partially...");
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
-        LT_ASSERT(LT_OK, lt_r_mem_data_write(&h, i, write_data, 100));
+        LT_LOG_INFO();
+        LT_LOG_INFO("Generating random data length < %d...", R_MEM_DATA_SIZE_MAX);
+        LT_ASSERT(LT_OK, lt_port_random_bytes(&random_data_size, 1));
+        random_data_size %= R_MEM_DATA_SIZE_MAX - 1; // range 0-442
+        random_data_size += 1; // range 1-443 (zero size would return fail)
+
+        LT_LOG_INFO("Generating %d random bytes for slot #%d...", random_data_size, i);
+        LT_ASSERT(LT_OK, lt_port_random_bytes(random_data, sizeof(random_data) / sizeof(uint32_t)));
+        memcpy(write_data, random_data, random_data_size);
+
+        LT_LOG_INFO("Writing to slot #%d...", i);
+        LT_ASSERT(LT_OK, lt_r_mem_data_write(&h, i, write_data, random_data_size));
+
+        LT_LOG_INFO("Reading slot #%d...", i);
         LT_ASSERT(LT_OK, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
-        LT_ASSERT(1, (read_data_size == 100));
-        LT_ASSERT(0, memcmp(r_mem_data, write_data, 100));
+
+        LT_LOG_INFO("Checking number of read bytes...");
+        LT_ASSERT(1, (read_data_size == random_data_size));
+
+        LT_LOG_INFO("Checking contents...");
+        LT_ASSERT(0, memcmp(r_mem_data, write_data, random_data_size));
     }
-    LT_LOG_INFO("All slots were successfuly written");
     LT_LOG_LINE();
 
-    LT_LOG_INFO("Loop through slots 0-%d, erasing them and checking if erased...", R_MEM_DATA_SLOT_MAX);
+    LT_LOG_INFO("Erasing all slots...");
     for (uint16_t i = 0; i <= R_MEM_DATA_SLOT_MAX; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Erasing slot #%d...", i);
         LT_ASSERT(LT_OK, lt_r_mem_data_erase(&h, i));
+
+        LT_LOG_INFO("Reading slot #%d (should fail)...", i);
         LT_ASSERT(LT_L3_R_MEM_DATA_READ_SLOT_EMPTY, lt_r_mem_data_read(&h, i, r_mem_data, &read_data_size));
+
+        LT_LOG_INFO("Checking number of read bytes (should be 0)...");
         LT_ASSERT(1, (read_data_size == 0));
     }
-    LT_LOG_INFO("All slots were successfuly erased");
     LT_LOG_LINE();
+
 
     LT_LOG_INFO("Aborting Secure Session");
     LT_ASSERT(LT_OK, lt_session_abort(&h));
