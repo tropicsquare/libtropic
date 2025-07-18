10e8a81aa35e08cc5c0ffafd63b1efcf8bd0b05c
Ondrej Golasowski <golasowski.o@gmail.com>
2025-07-18 10:21:09 +0200

    refactor(functional tests): use LT_TEST_ASSERT in tests.

--- tests/functional/lt_test_rev_erase_r_config.c
+++ tests/functional/lt_test_rev_erase_r_config.c
@@ -26,46 +26,46 @@ void lt_test_rev_erase_r_config(void)
     struct lt_config_t r_config, r_config_backup;
 
     LT_LOG_INFO("Initializing handle");
-    LT_ASSERT(LT_OK, lt_init(&h));
+    LT_TEST_ASSERT(LT_OK, lt_init(&h));
 
     LT_LOG_INFO("Starting Secure Session with key %d", PAIRING_KEY_SLOT_INDEX_0);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
+    LT_TEST_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
     LT_LOG_LINE();
 
     LT_LOG_INFO("Backing up the whole R config:");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config_backup));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config_backup));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config_backup.obj[i]);
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Erasing R config");
-    LT_ASSERT(LT_OK, lt_r_config_erase(&h));
+    LT_TEST_ASSERT(LT_OK, lt_r_config_erase(&h));
 
     LT_LOG_INFO("Reading the whole R config");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config.obj[i]);
         LT_LOG_INFO("Checking if it was erased");
-        LT_ASSERT(1, ((uint32_t)0xFFFFFFFF == r_config.obj[i]));
+        LT_TEST_ASSERT(1, ((uint32_t)0xFFFFFFFF == r_config.obj[i]));
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Restoring the whole R config");
-    LT_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_backup));
+    LT_TEST_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_backup));
 
     LT_LOG_INFO("Reading the whole R config");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config.obj[i]);
         LT_LOG_INFO("Checking if it was restored");
-        LT_ASSERT(1, (r_config_backup.obj[i] == r_config.obj[i]));
+        LT_TEST_ASSERT(1, (r_config_backup.obj[i] == r_config.obj[i]));
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Aborting Secure Session");
-    LT_ASSERT(LT_OK, lt_session_abort(&h));
+    LT_TEST_ASSERT(LT_OK, lt_session_abort(&h));
 
     LT_LOG_INFO("Deinitializing handle");
-    LT_ASSERT(LT_OK, lt_deinit(&h));
+    LT_TEST_ASSERT(LT_OK, lt_deinit(&h));
 }
--- tests/functional/lt_test_rev_write_r_config.c
+++ tests/functional/lt_test_rev_write_r_config.c
@@ -28,67 +28,67 @@ void lt_test_rev_write_r_config(void)
     struct lt_config_t r_config_random, r_config, r_config_backup;
 
     LT_LOG_INFO("Initializing handle");
-    LT_ASSERT(LT_OK, lt_init(&h));
+    LT_TEST_ASSERT(LT_OK, lt_init(&h));
 
     LT_LOG_INFO("Creating randomized R config for testing");
-    LT_ASSERT(LT_OK, lt_port_random_bytes(r_config_random.obj, LT_CONFIG_OBJ_CNT));
+    LT_TEST_ASSERT(LT_OK, lt_port_random_bytes(r_config_random.obj, LT_CONFIG_OBJ_CNT));
 
     LT_LOG_INFO("Starting Secure Session with key %d", PAIRING_KEY_SLOT_INDEX_0);
-    LT_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
+    LT_TEST_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
     LT_LOG_LINE();
 
     LT_LOG_INFO("Backing up the whole R config:");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config_backup));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config_backup));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config_backup.obj[i]);
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Writing the whole R config");
-    LT_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_random));
+    LT_TEST_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_random));
 
     LT_LOG_INFO("Reading the whole R config");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config.obj[i]);
         LT_LOG_INFO("Checking if it was written");
-        LT_ASSERT(1, (r_config.obj[i] == r_config_random.obj[i]));
+        LT_TEST_ASSERT(1, (r_config.obj[i] == r_config_random.obj[i]));
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Writing the whole R config again (should fail)");
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
-        LT_ASSERT(LT_L3_FAIL, lt_r_config_write(&h, cfg_desc_table[i].addr, r_config_backup.obj[i]));
+        LT_TEST_ASSERT(LT_L3_FAIL, lt_r_config_write(&h, cfg_desc_table[i].addr, r_config_backup.obj[i]));
     }
 
     LT_LOG_INFO("Reading the whole R config");
     memset(r_config.obj, 0, sizeof(r_config.obj));
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config.obj[i]);
         LT_LOG_INFO("Checking if it was not rewritten");
-        LT_ASSERT(1, (r_config.obj[i] == r_config_random.obj[i]));
+        LT_TEST_ASSERT(1, (r_config.obj[i] == r_config_random.obj[i]));
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Erasing R config before restoring (needed to be able to write again)");
-    LT_ASSERT(LT_OK, lt_r_config_erase(&h));
+    LT_TEST_ASSERT(LT_OK, lt_r_config_erase(&h));
 
     LT_LOG_INFO("Restoring the whole R config");
-    LT_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_backup));
+    LT_TEST_ASSERT(LT_OK, write_whole_R_config(&h, &r_config_backup));
 
     LT_LOG_INFO("Reading the whole R config");
-    LT_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
+    LT_TEST_ASSERT(LT_OK, read_whole_R_config(&h, &r_config));
     for (int i = 0; i < LT_CONFIG_OBJ_CNT; i++) {
         LT_LOG_INFO("%s: 0x%08x", cfg_desc_table[i].desc, (unsigned int)r_config.obj[i]);
         LT_LOG_INFO("Checking if it was restored");
-        LT_ASSERT(1, (r_config_backup.obj[i] == r_config.obj[i]));
+        LT_TEST_ASSERT(1, (r_config_backup.obj[i] == r_config.obj[i]));
     }
     LT_LOG_LINE();
 
     LT_LOG_INFO("Aborting Secure Session");
-    LT_ASSERT(LT_OK, lt_session_abort(&h));
+    LT_TEST_ASSERT(LT_OK, lt_session_abort(&h));
 
     LT_LOG_INFO("Deinitializing handle");
-    LT_ASSERT(LT_OK, lt_deinit(&h));
+    LT_TEST_ASSERT(LT_OK, lt_deinit(&h));
 }
