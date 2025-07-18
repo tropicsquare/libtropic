a9c7789ac7d474b21cbab1fdebdd70265d38de1a
Ondrej Golasowski <golasowski.o@gmail.com>
2025-07-18 15:25:37 +0200

    refactor: moved assert fail handler to function.

--- tests/functional/lt_test_common.c
+++ tests/functional/lt_test_common.c
@@ -7,5 +7,17 @@
  */
 
 #include <stdlib.h>
+#include "libtropic_functional_tests.h"
 
 void (*lt_test_cleanup_function)(void) = NULL;
+
+void lt_assert_fail_handler() {
+    if (lt_test_cleanup_function != NULL) {
+        LT_LOG_INFO("Cleanup started.");
+        lt_test_cleanup_function();
+        LT_LOG_INFO("Cleanup finished.");
+    } else {
+        LT_LOG_INFO("Cleanup function not defined -- skipped cleaning.");
+    }
+    LT_FINISH_TEST();
+}
