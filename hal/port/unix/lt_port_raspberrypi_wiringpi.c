2e82a147db2c8f9b598cf779cd744231d94b07cd
Jan Medek <czmedexs@gmail.com>
2025-07-21 16:15:47 +0200

    docs(lt_port_raspberrypi_wiringpi): fix @file name in the doxygen header

--- hal/port/unix/lt_port_raspberrypi_wiringpi.c
+++ hal/port/unix/lt_port_raspberrypi_wiringpi.c
@@ -1,5 +1,5 @@
 /**
- * @file lt_port_raspberrypi_spi.c
+ * @file lt_port_raspberrypi_wiringpi.c
  * @author Tropic Square s.r.o.
  * @brief  L1 implementation for raspberrypi. Expects wiringPi library to be
  * installed on Raspberry Pi, for more info about this library check https://github.com/WiringPi/WiringPi/tree/master
