f87160a56be6597c3d8ae6ec7a960d83dc2150b0
Ondrej Golasowski <golasowski.o@gmail.com>
2025-07-22 08:26:02 +0200

    refactor(types): removed u8, u16, u32 types.

--- src/lt_l2_api_structs.h
+++ src/lt_l2_api_structs.h
@@ -53,19 +53,19 @@
  * FW.
  */
 struct lt_l2_get_info_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
     /**
      * @brief
      * The Identifier of the requested object.
      */
-    u8 object_id;
+    uint8_t object_id;
     /**
      * @brief
      * The index of the 128 Byte long block to request
      */
-    u8 block_index;
-    u8 crc[2]; /**< Checksum */
+    uint8_t block_index;
+    uint8_t crc[2]; /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -90,15 +90,15 @@ STATIC_ASSERT(
  * FW.
  */
 struct lt_l2_get_info_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
     /**
      * @brief
      * The data content of the requested object block.
      */
-    u8 object[128];
-    u8 crc[2]; /**< Checksum */
+    uint8_t object[128];
+    uint8_t crc[2]; /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -136,21 +136,21 @@ STATIC_ASSERT(
  * Channel Mode).
  */
 struct lt_l2_handshake_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
     /**
      * @brief
      * The Host MCU's Ephemeral X25519 public key. A little endian encoding of the x-coordinate from the public
      * Curve25519 point.
      */
-    u8 e_hpub[32]; /**< Ephemeral Key of Host MCU. */
+    uint8_t e_hpub[32]; /**< Ephemeral Key of Host MCU. */
     /**
      * @brief
      * The index of the Pairing Key slot to establish a Secure Channel Session with (TROPIC01 fetches $S_{HiPub}$ from
      * the Pairing Key slot specified in this field).
      */
-    u8 pkey_index; /**< Pairing Key slot */
-    u8 crc[2];     /**< Checksum */
+    uint8_t pkey_index; /**< Pairing Key slot */
+    uint8_t crc[2];     /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -172,20 +172,20 @@ STATIC_ASSERT(
  * Channel Mode).
  */
 struct lt_l2_handshake_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
     /**
      * @brief
      * TROPIC01's X25519 Ephemeral key.
      */
-    u8 e_tpub[32]; /**< Ephemeral Key of TROPIC01. */
+    uint8_t e_tpub[32]; /**< Ephemeral Key of TROPIC01. */
     /**
      * @brief
      * The Secure Channel Handshake Authentication Tag.
      */
-    u8 t_tauth[16]; /**< Authentication Tag */
-    u8 crc[2];      /**< Checksum */
+    uint8_t t_tauth[16]; /**< Authentication Tag */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -218,11 +218,11 @@ STATIC_ASSERT(
  * Request to execute an L3 Command.
  */
 struct lt_l2_encrypted_cmd_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
     /** Contains a chunk of encrypted command */
     uint8_t l3_chunk[255];
-    u8 crc[2]; /**< Checksum */
+    uint8_t crc[2]; /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -242,16 +242,16 @@ STATIC_ASSERT(
  * Request to execute an L3 Command.
  */
 struct lt_l2_encrypted_cmd_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
     /**
      * @brief
      * The size of the RES_CIPHERTEXT L3 Field in bytes.
      */
     /** Contains a chunk of encrypted command */
     uint8_t l3_chunk[255];
-    u8 crc[2]; /**< Checksum */
+    uint8_t crc[2]; /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -280,9 +280,9 @@ STATIC_ASSERT(
  * Request to abort current Secure Channel Session and execution of L3 command (TROPIC01 moves to Idle Mode).
  */
 struct lt_l2_encrypted_session_abt_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
-    u8 crc[2];  /**< Checksum */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
+    uint8_t crc[2];  /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -301,10 +301,10 @@ STATIC_ASSERT(
  * Request to abort current Secure Channel Session and execution of L3 command (TROPIC01 moves to Idle Mode).
  */
 struct lt_l2_encrypted_session_abt_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -332,9 +332,9 @@ STATIC_ASSERT(
  * Request for TROPIC01 to resend the last L2 Response.
  */
 struct lt_l2_resend_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
-    u8 crc[2];  /**< Checksum */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
+    uint8_t crc[2];  /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -353,10 +353,10 @@ STATIC_ASSERT(
  * Request for TROPIC01 to resend the last L2 Response.
  */
 struct lt_l2_resend_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -388,14 +388,14 @@ STATIC_ASSERT(
  * Request for TROPIC01 to go to Sleep Mode or Deep Sleep Mode.
  */
 struct lt_l2_sleep_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
     /**
      * @brief
      * The type of Sleep mode TROPIC01 moves to.
      */
-    u8 sleep_kind; /**< Sleep Kind */
-    u8 crc[2];     /**< Checksum */
+    uint8_t sleep_kind; /**< Sleep Kind */
+    uint8_t crc[2];     /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -415,10 +415,10 @@ STATIC_ASSERT(
  * Request for TROPIC01 to go to Sleep Mode or Deep Sleep Mode.
  */
 struct lt_l2_sleep_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -450,10 +450,10 @@ STATIC_ASSERT(
  * Request for TROPIC01 to reset.
  */
 struct lt_l2_startup_req_t {
-    u8 req_id;     /**< Request ID byte */
-    u8 req_len;    /**< Length byte */
-    u8 startup_id; /**< The request ID */
-    u8 crc[2];     /**< Checksum */
+    uint8_t req_id;     /**< Request ID byte */
+    uint8_t req_len;    /**< Length byte */
+    uint8_t startup_id; /**< The request ID */
+    uint8_t crc[2];     /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -473,10 +473,10 @@ STATIC_ASSERT(
  * Request for TROPIC01 to reset.
  */
 struct lt_l2_startup_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -519,12 +519,12 @@ STATIC_ASSERT(
  * NOTE: Write only to the correctly erased bank (see Mutable_FW_Erase_Req).
  */
 struct lt_l2_mutable_fw_update_req_t {
-    u8 req_id;    /**< Request ID byte */
-    u8 req_len;   /**< Length byte */
-    u8 bank_id;   /**< The Identifier of the bank to write in. */
-    u16 offset;   /**< The offset of the specific bank to write the chunk */
-    u8 data[248]; /**< The binary data to write. Data size should be a multiple of 4. */
-    u8 crc[2];    /**< Checksum */
+    uint8_t req_id;    /**< Request ID byte */
+    uint8_t req_len;   /**< Length byte */
+    uint8_t bank_id;   /**< The Identifier of the bank to write in. */
+    uint16_t offset;   /**< The offset of the specific bank to write the chunk */
+    uint8_t data[248]; /**< The binary data to write. Data size should be a multiple of 4. */
+    uint8_t crc[2];    /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -549,10 +549,10 @@ STATIC_ASSERT(
  * NOTE: Write only to the correctly erased bank (see Mutable_FW_Erase_Req).
  */
 struct lt_l2_mutable_fw_update_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -589,10 +589,10 @@ STATIC_ASSERT(
  * Supported only in Start-up mode (i.e. after Startup_Req with MAINTENANCE_REBOOT).
  */
 struct lt_l2_mutable_fw_erase_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
-    u8 bank_id; /**< The Identifier of the bank to erase. The same choices as above. */
-    u8 crc[2];  /**< Checksum */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
+    uint8_t bank_id; /**< The Identifier of the bank to erase. The same choices as above. */
+    uint8_t crc[2];  /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -613,10 +613,10 @@ STATIC_ASSERT(
  * Supported only in Start-up mode (i.e. after Startup_Req with MAINTENANCE_REBOOT).
  */
 struct lt_l2_mutable_fw_erase_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
-    u8 crc[2];      /**< Checksum */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
+    uint8_t crc[2];      /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -644,9 +644,9 @@ STATIC_ASSERT(
  * Get log from FW running on RISCV CPU.
  */
 struct lt_l2_get_log_req_t {
-    u8 req_id;  /**< Request ID byte */
-    u8 req_len; /**< Length byte */
-    u8 crc[2];  /**< Checksum */
+    uint8_t req_id;  /**< Request ID byte */
+    uint8_t req_len; /**< Length byte */
+    uint8_t crc[2];  /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
@@ -665,15 +665,15 @@ STATIC_ASSERT(
  * Get log from FW running on RISCV CPU.
  */
 struct lt_l2_get_log_rsp_t {
-    u8 chip_status; /**< CHIP_STATUS byte */
-    u8 status;      /**< L2 status byte */
-    u8 rsp_len;     /**< Length of incoming data */
+    uint8_t chip_status; /**< CHIP_STATUS byte */
+    uint8_t status;      /**< L2 status byte */
+    uint8_t rsp_len;     /**< Length of incoming data */
     /**
      * @brief
      * Log message of RISCV FW.
      */
-    u8 log_msg[255]; /**< Log message */
-    u8 crc[2];       /**< Checksum */
+    uint8_t log_msg[255]; /**< Log message */
+    uint8_t crc[2];       /**< Checksum */
 } __attribute__((packed));
 
 // clang-format off
