c16caa0aa9040ddf9d37803dcdd0c33df1f5017b
Jan Medek <czmedexs@gmail.com>
2025-07-24 09:31:42 +0200

    test(lt_test_rev_eddsa_sign): refactor, add cleanup

--- tests/functional/lt_test_rev_eddsa_sign.c
+++ tests/functional/lt_test_rev_eddsa_sign.c
@@ -5,108 +5,163 @@
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
  */
-#include "inttypes.h"
+
 #include "libtropic.h"
 #include "libtropic_common.h"
 #include "libtropic_functional_tests.h"
 #include "libtropic_logging.h"
+#include "libtropic_port.h"
+#include "lt_l3_api_structs.h"
 #include "string.h"
 
-uint8_t attestation_key[32]
-    = {0x22, 0x57, 0xa8, 0x2f, 0x85, 0x8f, 0x13, 0x32, 0xfa, 0x0f, 0xf6, 0x0c, 0x76, 0x29, 0x42, 0x70,
-       0xa9, 0x58, 0x9d, 0xfd, 0x47, 0xa5, 0x23, 0x78, 0x18, 0x4d, 0x2d, 0x38, 0xf0, 0xa7, 0xc4, 0x01};
+// Pre-generated key for testing using OpenSSL
+uint8_t priv_test_key[]
+    = {0x42, 0xb2, 0xee, 0x0e, 0x9b, 0xb5, 0x72, 0x50, 0x6e, 0xda, 0x06, 0x3e, 0xcb, 0x98, 0x43, 0x51,
+       0x6f, 0xeb, 0xe6, 0x5f, 0x94, 0x3e, 0xf1, 0xb6, 0x0c, 0x74, 0x6d, 0x4c, 0x46, 0x83, 0xde, 0x15};
 
+// Shared with cleanup function
+lt_handle_t h;
+
+lt_ret_t lt_test_rev_eddsa_sign_cleanup(void)
+{
+    lt_ret_t ret;
+    uint8_t read_pub_key[64];
+    lt_ecc_curve_type_t curve;
+    ecc_key_origin_t origin;
+
+    LT_LOG_INFO("Starting secure session with slot %d", PAIRING_KEY_SLOT_INDEX_0);
+    ret = verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0);
+    if (LT_OK != ret) {
+        LT_LOG_ERROR("Failed to establish secure session, ret=%s", lt_ret_verbose(ret));
+        return ret;
+    }
+
+    LT_LOG_INFO("Erasing all ECC key slots");
+    for (uint8_t i = 0; i <= ECC_SLOT_31; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Erasing slot #%d", i);
+        ret = lt_ecc_key_erase(&h, i);
+        if (LT_OK != ret) {
+            LT_LOG_ERROR("Failed to erase slot, ret=%s", lt_ret_verbose(ret));
+            return ret;
+        }
+
+        LT_LOG_INFO("Reading slot #%d (should fail)", i);
+        ret = lt_ecc_key_read(&h, i, read_pub_key, &curve, &origin);
+        if (LT_L3_ECC_INVALID_KEY != ret) {
+            LT_LOG_ERROR("Return value is not LT_L3_ECC_INVALID_KEY, ret=%s", lt_ret_verbose(ret));
+            return ret;
+        }
+    }
+
+    LT_LOG_INFO("Aborting secure session");
+    ret = lt_session_abort(&h);
+    if (LT_OK != ret) {
+        LT_LOG_ERROR("Failed to abort secure session, ret=%s", lt_ret_verbose(ret));
+        return ret;
+    }
+
+    LT_LOG_INFO("Deinitializing handle");
+    ret = lt_deinit(&h);
+    if (LT_OK != ret) {
+        LT_LOG_ERROR("Failed to deinitialize handle, ret=%s", lt_ret_verbose(ret));
+        return ret;
+    }
+
+    return LT_OK;
+}
 
 void lt_test_rev_eddsa_sign(void)
 {
-    LT_LOG(
-        "  "
-        "------------------------------------------------------------------------------------------------------------"
-        "-");
-    LT_LOG(
-        "  -------- lt_test_rev_eddsa_sign() "
-        "--------------------------------------------------------------------------------");
-    LT_LOG(
-        "  "
-        "------------------------------------------------------------------------------------------------------------"
-        "-");
+    LT_LOG_INFO("----------------------------------------------");
+    LT_LOG_INFO("lt_test_rev_eddsa_sign()");
+    LT_LOG_INFO("----------------------------------------------");
 
-    lt_handle_t h = {0};
 #if LT_SEPARATE_L3_BUFF
     uint8_t l3_buffer[L3_FRAME_MAX_SIZE] __attribute__((aligned(16))) = {0};
     h.l3.buff = l3_buffer;
     h.l3.buff_len = sizeof(l3_buffer);
 #endif
+    uint8_t read_pub_key[32], msg_to_sign[LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX], rs[64];
+    lt_ecc_curve_type_t curve;
+    ecc_key_origin_t origin;
+    uint32_t random_data[LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX / sizeof(uint32_t)], random_data_size;
 
-    LT_LOG("%s", "Initialize handle");
+    LT_LOG_INFO("Initializing handle");
     LT_TEST_ASSERT(LT_OK, lt_init(&h));
 
-    // Ping with SH0
-    LT_LOG("%s with %d", "verify_chip_and_start_secure_session()", PAIRING_KEY_SLOT_INDEX_0);
+    LT_LOG_INFO("Starting Secure Session with key %d", PAIRING_KEY_SLOT_INDEX_0);
     LT_TEST_ASSERT(LT_OK, verify_chip_and_start_secure_session(&h, sh0priv, sh0pub, PAIRING_KEY_SLOT_INDEX_0));
+    LT_LOG_LINE();
 
-    // Erase all keys, used in case previous run failed in the middle
-    // for(uint16_t i=0; i<32; i++) {
-    //    LT_LOG("lt_ecc_key_erase() slot        n.%d  ", i);
-    //    LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_erase(&h, i)));
-    //}
+    lt_test_cleanup_function = &lt_test_rev_eddsa_sign_cleanup;
 
-    LT_LOG("---------------- Loop through all key slots, ed25519, stored key ----------------");
+    LT_LOG_INFO("Test EDDSA_Sign with stored key...");
+    for (uint8_t i = 0; i <= ECC_SLOT_31; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Testing signing with ECC key slot #%d...", i);
 
-    for (uint16_t i = 0; i < 32; i++) {
-        LT_LOG("lt_ecc_key_store() slot        n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_store(&h, i, CURVE_ED25519, attestation_key)));
+        LT_LOG_INFO("Storing private key pre-generated using Ed25519 curve...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_store(&h, i, CURVE_ED25519, priv_test_key));
 
-        LT_LOG("lt_ecc_key_read() slot         n.%d  ", i);
-        uint8_t slot_0_pubkey[64];
-        lt_ecc_curve_type_t curve;
-        ecc_key_origin_t origin;
-        LT_TEST_ASSERT(lt_ecc_key_read(&h, i, slot_0_pubkey, &curve, &origin), LT_OK);
+        LT_LOG_INFO("Reading the stored public key...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_read(&h, i, read_pub_key, &curve, &origin));
+        
+        LT_LOG_INFO("Generating random data length <= %d...", LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX);
+        LT_TEST_ASSERT(LT_OK, lt_port_random_bytes(&random_data_size, 1));
+        random_data_size %= LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX + 1; // 0-4096
 
-        LT_LOG("lt_ecc_eddsa_sign() slot       n.%d  ", i);
-        uint8_t msg[] = {'a', 'h', 'o', 'j'};
-        uint8_t rs[64];
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_eddsa_sign(&h, i, msg, 4, rs)));
+        LT_LOG_INFO("Generating random message with length %d for signing...", random_data_size);
+        LT_TEST_ASSERT(LT_OK, lt_port_random_bytes(random_data, sizeof(random_data) / sizeof(uint32_t)));
+        memcpy(msg_to_sign, random_data, random_data_size);
 
-        LT_LOG("lt_ecc_eddsa_sig_verify() slot n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_eddsa_sig_verify(msg, 4, slot_0_pubkey, rs)));
+        LT_LOG_INFO("Signing message...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_eddsa_sign(&h, i, msg_to_sign, random_data_size, rs));
 
-        LT_LOG("lt_ecc_key_erase() slot        n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_erase(&h, i)));
+        LT_LOG_INFO("Verifying signature...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_eddsa_sig_verify(msg_to_sign, random_data_size, read_pub_key, rs));
 
-        LT_LOG("");
+        LT_LOG_INFO("Erasing the slot...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_erase(&h, i));
     }
+    LT_LOG_LINE();
 
-    LT_LOG("---------------- Loop through all key slots, ed25519, generated key ----------------");
+    LT_LOG_INFO("Test EDDSA_Sign with generated key...");
+    for (uint8_t i = 0; i <= ECC_SLOT_31; i++) {
+        LT_LOG_INFO();
+        LT_LOG_INFO("Testing signing with ECC key slot #%d...", i);
 
-    for (int i = 0; i < 32; i++) {
-        LT_LOG("lt_ecc_key_generate() slot     n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_generate(&h, i, CURVE_ED25519)));
+        LT_LOG_INFO("Generating private key using Ed25519 curve...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_generate(&h, i, CURVE_ED25519));
 
-        LT_LOG("lt_ecc_key_read() slot         n.%d  ", i);
-        uint8_t slot_0_pubkey[64];
-        lt_ecc_curve_type_t curve;
-        ecc_key_origin_t origin;
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_read(&h, i, slot_0_pubkey, &curve, &origin)));
+        LT_LOG_INFO("Reading the generated public key...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_read(&h, i, read_pub_key, &curve, &origin));
+        
+        LT_LOG_INFO("Generating random data length <= %d...", LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX);
+        LT_TEST_ASSERT(LT_OK, lt_port_random_bytes(&random_data_size, 1));
+        random_data_size %= LT_L3_EDDSA_SIGN_CMD_MSG_LEN_MAX + 1; // 0-4096
 
-        LT_LOG("lt_ecc_eddsa_sign() slot       n.%d  ", i);
-        uint8_t msg[] = {'a', 'h', 'o', 'j'};
-        uint8_t rs[64];
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_eddsa_sign(&h, i, msg, 4, rs)));
+        LT_LOG_INFO("Generating random message with length %d for signing...", random_data_size);
+        LT_TEST_ASSERT(LT_OK, lt_port_random_bytes(random_data, sizeof(random_data) / sizeof(uint32_t)));
+        memcpy(msg_to_sign, random_data, random_data_size);
 
-        LT_LOG("lt_ecc_eddsa_sig_verify() slot n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_eddsa_sig_verify(msg, 4, slot_0_pubkey, rs)));
+        LT_LOG_INFO("Signing message...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_eddsa_sign(&h, i, msg_to_sign, random_data_size, rs));
 
-        LT_LOG("lt_ecc_key_erase() slot        n.%d  ", i);
-        LT_LOG_RESULT("%s", lt_ret_verbose(lt_ecc_key_erase(&h, i)));
+        LT_LOG_INFO("Verifying signature...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_eddsa_sig_verify(msg_to_sign, random_data_size, read_pub_key, rs));
 
-        LT_LOG("");
+        LT_LOG_INFO("Erasing the slot...");
+        LT_TEST_ASSERT(LT_OK, lt_ecc_key_erase(&h, i));
     }
+    LT_LOG_LINE();
 
-    LT_LOG("%s", "lt_session_abort()");
+    // Cleanup not needed anymore, all slots were erased
+    lt_test_cleanup_function = NULL;
+
+    LT_LOG_INFO("Aborting Secure Session");
     LT_TEST_ASSERT(LT_OK, lt_session_abort(&h));
 
-    // Deinit handle
-    LT_LOG("%s", "lt_deinit()");
+    LT_LOG_INFO("Deinitializing handle");
     LT_TEST_ASSERT(LT_OK, lt_deinit(&h));
 }
