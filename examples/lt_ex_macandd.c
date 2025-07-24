f5cc4eb622378c05283452ce543984cf37c52388
Jan Medek <czmedexs@gmail.com>
2025-07-24 11:00:59 +0200

    fix(lt_ex_macandd): fix arguments in lt_r_mem_data_read (API changed)

--- examples/lt_ex_macandd.c
+++ examples/lt_ex_macandd.c
@@ -279,7 +279,8 @@ static lt_ret_t lt_PIN_check(lt_handle_t *h, const uint8_t *PIN, const uint8_t P
     memcpy(kdf_input_buff + PIN_size, add, add_size);
 
     // Load M&D data from TROPIC01's R memory
-    lt_ret_t ret = lt_r_mem_data_read(h, R_MEM_DATA_SLOT_MACANDD, (uint8_t *)&nvm, sizeof(nvm));
+    uint16_t read_size;
+    lt_ret_t ret = lt_r_mem_data_read(h, R_MEM_DATA_SLOT_MACANDD, (uint8_t *)&nvm, &read_size);
     if (ret != LT_OK) {
         goto exit;
     }
