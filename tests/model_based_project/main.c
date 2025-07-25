1bf8a56053e44b7c43a121903a6f4aea5ec0d56f
Ondrej Golasowski <golasowski.o@gmail.com>
2025-07-25 13:27:05 +0200

    refactor(model project): changed compilation of examples in model project.

--- tests/model_based_project/main.c
+++ tests/model_based_project/main.c
@@ -11,23 +11,14 @@
 
 int main(void)
 {
-// Full examples
-#ifdef LT_EX_HELLO_WORLD
-    /*int*/ lt_ex_hello_world();
-#endif
-#ifdef LT_EX_HELLO_WORLD_SEPARATE_API
-    /*int*/ lt_ex_hello_world_separate_API();
-#endif
-#ifdef LT_EX_HW_WALLET
-    /*int*/ lt_ex_hardware_wallet();
-#endif
-#ifdef LT_EX_MACANDD
-    /*int*/ lt_ex_macandd();
-#endif
 
-#ifdef LT_BUILD_TESTS
-#include "lt_test_registry.c.inc"
-#endif
+    #ifdef LT_BUILD_EXAMPLES
+    #include "lt_ex_registry.c.inc"
+    #endif
+
+    #ifdef LT_BUILD_TESTS
+    #include "lt_test_registry.c.inc"
+    #endif
 
     return 0;
 }
