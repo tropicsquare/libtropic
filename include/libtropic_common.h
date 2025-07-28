ff85d50dc5240c4783ff661cf43936f9a6dbb0a9
Jan Medek <czmedexs@gmail.com>
2025-07-28 16:16:13 +0200

    docs(L3_PACKET_MAX_SIZE): describe the number 13

--- include/libtropic_common.h
+++ include/libtropic_common.h
@@ -63,7 +63,11 @@ typedef uint32_t u32;
 
 /** @brief Maximum size of l3 ciphertext (or decrypted l3 packet) */
 #define L3_CYPHERTEXT_MAX_SIZE (L3_CMD_ID_SIZE + L3_CMD_DATA_SIZE_MAX)
-/** @brief Max size of one unit of transport on l3 layer */
+/**
+ * @brief Max size of one unit of transport on l3 layer
+ * 
+ * The number 13 is given by the longest possible padding, which is given by the EDDSA_Sign command.
+ */
 #define L3_PACKET_MAX_SIZE (L3_RES_SIZE_SIZE + L3_CYPHERTEXT_MAX_SIZE + 13 + L3_TAG_SIZE)
 
 //--------------------------------------------------------------------------------------------------------------------//
