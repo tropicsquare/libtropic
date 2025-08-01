a868bbf5f692ba171c7fe360bcb3fbf50cb2ebb4
Jan Medek <czmedexs@gmail.com>
2025-08-01 11:46:09 +0200

    fix(lt_out__session_start): randomize uint32_t buffer and copy it into uint8_t for safer portability

--- src/lt_l3.c
+++ src/lt_l3.c
@@ -34,12 +34,13 @@ lt_ret_t lt_out__session_start(lt_handle_t *h, const pkey_index_t pkey_index, se
     memset(h->l3.decryption_IV, 0, sizeof(h->l3.decryption_IV));
 
     // Create ephemeral host keys
-    uint32_t random_data[8];
-    lt_ret_t ret = lt_random_bytes(random_data, 8);
-    memcpy(state->ehpriv, random_data, 8);
+    uint8_t random_data_size = sizeof(state->ehpriv) / 4;
+    uint32_t random_data[random_data_size];
+    lt_ret_t ret = lt_random_bytes(random_data, random_data_size);
     if (ret != LT_OK) {
         return ret;
     }
+    memcpy(state->ehpriv, random_data, random_data_size);
     lt_X25519_scalarmult(state->ehpriv, state->ehpub);
 
     // Setup a request pointer to l2 buffer, which is placed in handle
