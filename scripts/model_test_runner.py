2f3aba6ea82c36db5d6618932445e6e61ccb082b
Jan Medek <czmedexs@gmail.com>
2025-08-06 16:11:38 +0200

    feat(model_test_runner): get logging cfg from model and modify it so it logs to a file

--- scripts/model_test_runner.py
+++ scripts/model_test_runner.py
@@ -39,13 +39,6 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "-l", "--model-log-cfg",
-        help="Path to the model logging configuration YAML file.",
-        type=pathlib.Path,
-        required=True
-    )
-
-    parser.add_argument(
         "-o", "--output-dir",
         help="Path to the directory where output should be saved.",
         type=pathlib.Path,
@@ -57,36 +50,47 @@ if __name__ == "__main__":
     # Save args
     test_path: pathlib.Path = args.test
     model_cfg_path: pathlib.Path = args.model_cfg
-    model_log_cfg_path: pathlib.Path = args.model_log_cfg
     output_path: pathlib.Path = args.output_dir
     test_name = test_path.stem
 
     # Create destination directory if it doesn't exist yet
     output_path.mkdir(parents=True, exist_ok=True)
 
-    # Load model logging configuration, modify logging file name and save
-    with model_log_cfg_path.open("r") as f:
-        model_log_cfg = yaml.safe_load(f)
-    
-    model_logging_file_path = output_path.joinpath(f"{test_name}_model_response").with_suffix(".log")
-    model_log_cfg["handlers"]["file"]["filename"] = str(model_logging_file_path)
+    # Get the logging configuration from the model so we can modify it
+    dump_logging_cfg_res = subprocess.run(
+        ["model_server", "dump-logging-cfg"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    # Transform the YAML output to a dictionary
+    model_log_cfg = yaml.safe_load(dump_logging_cfg_res.stdout)
+    # Change the default handler to a file
+    model_log_cfg["handlers"]["default"]["class"] = "logging.FileHandler"
+    model_log_cfg["handlers"]["default"]["filename"] = str(output_path.joinpath(f"{test_name}_model_response").with_suffix(".log"))
+    model_log_cfg["handlers"]["default"]["mode"] = "w"
+    model_log_cfg["handlers"]["default"]["encoding"] = "utf8"
+    # Change logging level to DEBUG
+    model_log_cfg["loggers"]["model"]["level"] = "DEBUG"
+    model_log_cfg["loggers"]["server"]["level"] = "DEBUG"
+    # Disable colors to prevent weird symbols in the .log file
+    model_log_cfg["formatters"]["default"]["use_colors"] = False
 
-    model_logging_cfg_modified_path = pathlib.Path("model_logging_cfg_modified.yml")
-    with model_logging_cfg_modified_path.open("w") as f:
+    model_log_cfg_path = pathlib.Path("model_log_cfg_path.yml")
+    with model_log_cfg_path.open("w") as f:
         yaml.dump(model_log_cfg, f, default_flow_style=False)
-
+    
     # Start the model server
     model_process = subprocess.Popen(
         [
             "model_server", "tcp",
             "-c", f"{str(model_cfg_path)}",
-            "-l", f"{str(model_logging_cfg_modified_path)}"
+            "-l", f"{str(model_log_cfg_path)}"
         ]
     )
 
     # Wait for model server to start
     if wait_for_server_start() == False:
-        model_logging_cfg_modified_path.unlink()
         print("Server did not start.")
         sys.exit(1)
 
@@ -112,5 +116,4 @@ if __name__ == "__main__":
         model_process.wait(timeout=10)
     except subprocess.TimeoutExpired:
         model_process.kill()
-    model_logging_cfg_modified_path.unlink()
     sys.exit(ret)
