af2d3d74efa121b77c7318ad430edfdb148e198a
andreondra <golasowski.o@gmail.com>
2025-08-07 15:34:39 +0200

    refactor(lt_port_unix_spi): cleaned up logging, identifiers, comments.

--- hal/port/unix/lt_port_unix_spi.c
+++ hal/port/unix/lt_port_unix_spi.c
@@ -2,6 +2,9 @@
  * @file lt_port_unix_spi.c
  * @author Evgeny Beysembaev <evgeny@contentwise.tech>
  * @brief Port for communication using Generic SPI and GPIO Linux UAPI.
+ * 
+ * @note The chip select (CS) pin is controlled separately using GPIO, as the protocol requires
+ *       manual handling of the chip select.
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
  */
@@ -30,61 +33,69 @@
 
 lt_ret_t lt_port_init(lt_l2_state_t *s2)
 {
-    uint32_t request_mode;
-    uint8_t spiBPW = 8;
-
-    lt_dev_unix_spi_t *device = (lt_dev_unix_spi_t *)(s2->device);
+    lt_dev_unix_spi_t *device        = (lt_dev_unix_spi_t *)(s2->device);
+    uint8_t            spiBPW        = 8;
+    uint32_t           request_mode;
 
     srand(device->rng_seed);
 
     LT_LOG_DEBUG("Initializing SPI...\n");
-    LT_LOG_DEBUG("SPI speed: %d", device->spi_speed);
-    LT_LOG_DEBUG("SPI device: %s", device->spi_dev);
+    LT_LOG_DEBUG("SPI speed: %d",   device->spi_speed);
+    LT_LOG_DEBUG("SPI device: %s",  device->spi_dev);
     LT_LOG_DEBUG("GPIO device: %s", device->gpio_dev);
     LT_LOG_DEBUG("GPIO CS pin: %d", device->gpio_cs_num);
 
     device->mode = SPI_MODE_0;
     device->fd = open(device->spi_dev, O_RDWR);
     if (device->fd < 0) {
-        LT_LOG_ERROR("can't open device");
+        LT_LOG_ERROR("Can't open device!");
         return LT_FAIL;
     }
 
     request_mode = device->mode;
     if (ioctl(device->fd, SPI_IOC_WR_MODE32, &device->mode) < 0) {
-        LT_LOG_ERROR("can't set spi mode");
+        LT_LOG_ERROR("Can't set SPI mode!");
+        close(device->fd);
         return LT_FAIL;
     }
-    /* RD is read what mode the device actually is in */
+    
+    // RD is read what mode the device actually is in.
     if (ioctl(device->fd, SPI_IOC_RD_MODE32, &device->mode) < 0) {
-        LT_LOG_ERROR("can't get spi mode");
+        LT_LOG_ERROR("Can't get SPI mode!");
+        close(device->fd);
         return LT_FAIL;
     }
-    if (request_mode != device->mode)
-        LT_LOG_ERROR("WARNING device does not support requested mode 0x%" PRIx32 "\n", request_mode);
+    if (request_mode != device->mode) {
+        LT_LOG_WARN("Device does not support requested mode 0x%" PRIx32, request_mode);
+    }
 
     if (ioctl(device->fd, SPI_IOC_WR_MAX_SPEED_HZ, &device->spi_speed) < 0) {
-        LT_LOG_ERROR("can't set max speed Hz");
+        LT_LOG_ERROR("Can't set max SPI speed.");
+        close(device->fd);
         return LT_FAIL;
     }
 
-    // CS is controlled separately
+    // CS is controlled separately.
     device->gpio_fd = open(device->gpio_dev, O_RDWR | O_CLOEXEC);
     if (device->gpio_fd < 0) {
-        LT_LOG_ERROR("can't open GPIO device");
+        LT_LOG_ERROR("Can't open GPIO device!");
+        close(device->fd);
         return LT_FAIL;
     }
 
     struct gpiochip_info info;
     if (ioctl(device->gpio_fd, GPIO_GET_CHIPINFO_IOCTL, &info) < 0) {
-        LT_LOG_ERROR("GPIO_GET_CHIPINFO_IOCTL");
+        LT_LOG_ERROR("GPIO_GET_CHIPINFO_IOCTL error!");
+        LT_LOG_ERROR("Error string: %s", strerror(errno));
+        close(device->fd);
+        close(device->gpio_fd);
         return LT_FAIL;
     }
 
-    LT_LOG_DEBUG("Device information:");
-    LT_LOG_DEBUG("[+] info.name = \"%s\"", info.name);
-    LT_LOG_DEBUG("[+] info.label = \"%s\"", info.label);
-    LT_LOG_DEBUG("[+] info.lines = \"%u\"", info.lines);
+    LT_LOG_DEBUG("GPIO chip information:");
+    LT_LOG_DEBUG("- info.name  = \"%s\"", info.name);
+    LT_LOG_DEBUG("- info.label = \"%s\"", info.label);
+    LT_LOG_DEBUG("- info.lines = \"%u\"", info.lines);
 
     memset(&device->gpioreq, 0, sizeof(device->gpioreq));
     device->gpioreq.offsets[0] = device->gpio_cs_num;
@@ -95,8 +106,10 @@ lt_ret_t lt_port_init(lt_l2_state_t *s2)
     device->gpioreq.config.attrs[0].mask = 1;
     device->gpioreq.config.attrs[0].attr.values = 1;  // initial value = 1
     if (ioctl(device->gpio_fd, GPIO_V2_GET_LINE_IOCTL, &device->gpioreq) < 0) {
-        LT_LOG_ERROR("GPIO_V2_GET_LINE_IOCTL\n");
-        LT_LOG_ERROR("Errno: %s\n", strerror(errno));
+        LT_LOG_ERROR("GPIO_V2_GET_LINE_IOCTL error!");
+        LT_LOG_ERROR("Error string: %s", strerror(errno));
+        close(device->fd);
+        close(device->gpio_fd);
         return LT_FAIL;
     }
 
@@ -107,8 +120,14 @@ lt_ret_t lt_port_deinit(lt_l2_state_t *s2)
 {
     lt_dev_unix_spi_t *device = (lt_dev_unix_spi_t *)(s2->device);
 
-    close(device->gpio_fd);
-    close(device->fd);
+    // We want to attempt to close both, even if one of them fails, hence storing the return val
+    // and checking later.
+    int gpio_close_ret = close(device->gpio_fd);
+    int spi_close_ret  = close(device->fd);
+
+    if (gpio_close_ret || spi_close_ret) {
+        return LT_FAIL;
+    }
     return LT_OK;
 }
 
@@ -120,7 +139,8 @@ lt_ret_t lt_port_spi_csn_low(lt_l2_state_t *s2)
     values.mask = 1;
     values.bits = 0;
     if (ioctl(device->gpioreq.fd, GPIO_V2_LINE_SET_VALUES_IOCTL, &values) < 0) {
-        LT_LOG_ERROR("GPIO_V2_LINE_SET_VALUES_IOCTL");
+        LT_LOG_ERROR("GPIO_V2_LINE_SET_VALUES_IOCTL error!");
+        LT_LOG_ERROR("Error string: %s", strerror(errno));
         return LT_FAIL;
     }
     return LT_OK;
@@ -134,7 +154,8 @@ lt_ret_t lt_port_spi_csn_high(lt_l2_state_t *s2)
     values.mask = 1;
     values.bits = 1;
     if (ioctl(device->gpioreq.fd, GPIO_V2_LINE_SET_VALUES_IOCTL, &values) < 0) {
-        LT_LOG_ERROR("GPIO_V2_LINE_SET_VALUES_IOCTL");
+        LT_LOG_ERROR("GPIO_V2_LINE_SET_VALUES_IOCTL error!");
+        LT_LOG_ERROR("Error string: %s", strerror(errno));
         return LT_FAIL;
     }
     return LT_OK;
@@ -147,14 +168,16 @@ lt_ret_t lt_port_spi_transfer(lt_l2_state_t *s2, uint8_t offset, uint16_t tx_dat
 
     int ret = 0;
     struct spi_ioc_transfer spi = {
-        .tx_buf = (unsigned long)s2->buff + offset,
-        .rx_buf = (unsigned long)s2->buff + offset,
-        .len = tx_data_length,
+        .tx_buf      = (unsigned long)s2->buff + offset,
+        .rx_buf      = (unsigned long)s2->buff + offset,
+        .len         = tx_data_length,
         .delay_usecs = 0,
     };
 
     ret = ioctl(device->fd, SPI_IOC_MESSAGE(1), &spi);
-    if (ret >= 0) return LT_OK;
+    if (ret >= 0) {
+        return LT_OK;
+    }
     return LT_FAIL;
 }
 
