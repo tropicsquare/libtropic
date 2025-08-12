040c5717da507a6d0a4b186dabef0e15aa89a0a6
Pavel Polach <pavel.polach@tropicsquare.com>
2025-08-12 19:14:44 +0200

    fix (examples/fw_update,chip_id) : Fix return values, fix inttypes

--- examples/lt_ex_fw_update.c
+++ examples/lt_ex_fw_update.c
@@ -3,26 +3,6 @@
  * @name Firmware update
  * @brief This code performs firmware update of TROPIC01 chip, works on both ABAB and ACAB silicon revisions.
  *
- * How to select the firmware for this example:
- *
- * The `TROPIC01_fw_update_files/` directory contains all officially released
- * and signed firmwares for the TROPIC01 chip, for all silicon revision.
- *
- * To specify which firmware to use for the update:
- *   1. Open the `CMakeLists.txt` file.
- *   2. Set the `LT_SILICON_REV` variable to your chip's silicon revision (e.g., "ABAB").
- *   3. Set the `LT_CPU_FW_VERSION` the desired versions.
- *
- * The build system uses these variables to place chosen firmware data into compiled binary, SPECT firmware data will be
- * chosen automatically.
- *
- * @note ACAB revision handles firmware banks differently than ABAB revision.
- *      For ACAB, the chip manages firmware banks internally, and passed `bank_id` is ignored.
- *      For ABAB, the chip requires the user to specify which bank to update, this might be defined by
- * FW_APP_UPDATE_BANK and FW_APP_UPDATE_SPECT in this file. With ABABs, sometimes, it may be necessary to erase both
- * banks to see expected behaviour, because chip always boots higher firmware version.
- *
- *
  * @author Tropic Square s.r.o.
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
@@ -50,11 +30,37 @@
 #define FW_APP_UPDATE_BANK FW_BANK_FW1
 #define FW_SPECT_UPDATE_BANK FW_BANK_SPECT1
 
+/**
+ * @brief Performs firmware update of TROPIC01 chip.
+ *
+ * How to select the firmware for this example:
+ *
+ * The `TROPIC01_fw_update_files/` directory contains all officially released
+ * and signed firmwares for the TROPIC01 chip, for all silicon revision.
+ *
+ * To specify which firmware to use for the update:
+ *   1. Open the `CMakeLists.txt` file.
+ *   2. Set the `LT_SILICON_REV` variable to your chip's silicon revision (e.g., "ABAB").
+ *   3. Set the `LT_CPU_FW_VERSION` the desired versions.
+ *
+ * The build system uses these variables to place chosen firmware data into compiled binary, SPECT firmware data will be
+ * chosen automatically.
+ *
+ * @note ACAB revision handles firmware banks differently than ABAB revision.
+ *      For ACAB, the chip manages firmware banks internally, and passed `bank_id` is ignored.
+ *      For ABAB, the chip requires the user to specify which bank to update, this might be defined by
+ * FW_APP_UPDATE_BANK and FW_APP_UPDATE_SPECT in this file. With ABABs, sometimes, it may be necessary to erase both
+ * banks to see expected behaviour, because chip always boots higher firmware version.
+ *
+ *
+ * @param h     Handle to the TROPIC01 chip.
+ * @return int  0 on success, -1 on failure.
+ */
 int lt_ex_fw_update(lt_handle_t *h)
 {
-    LT_LOG("\t=======================================================================");
-    LT_LOG("\t=====  TROPIC01 FW update                                           ===");
-    LT_LOG("\t=======================================================================");
+    LT_LOG_INFO("\t=======================================================================");
+    LT_LOG_INFO("\t=====  TROPIC01 FW update                                           ===");
+    LT_LOG_INFO("\t=======================================================================");
 
     lt_ret_t ret = LT_FAIL;
 
@@ -71,17 +77,17 @@ int lt_ex_fw_update(lt_handle_t *h)
     }
 
     if (h->l2.mode == LT_MODE_MAINTENANCE) {
-        LT_LOG("  Chip is executing bootloader");
+        LT_LOG_INFO("  Chip is executing bootloader");
 
-        LT_LOG("lt_do_mutable_fw_update() APP                 %s",
-               lt_ret_verbose(lt_do_mutable_fw_update(h, fw_CPU, sizeof(fw_CPU),
-                                                      FW_APP_UPDATE_BANK)));  // Update CPU firmware bank
-        LT_LOG("lt_do_mutable_fw_update() SPECT               %s",
-               lt_ret_verbose(lt_do_mutable_fw_update(h, fw_SPECT, sizeof(fw_SPECT),
-                                                      FW_SPECT_UPDATE_BANK)));  // Update SPECT firmware bank
+        LT_LOG_INFO("lt_do_mutable_fw_update() APP                 %s",
+                    lt_ret_verbose(lt_do_mutable_fw_update(h, fw_CPU, sizeof(fw_CPU),
+                                                           FW_APP_UPDATE_BANK)));  // Update CPU firmware bank
+        LT_LOG_INFO("lt_do_mutable_fw_update() SPECT               %s",
+                    lt_ret_verbose(lt_do_mutable_fw_update(h, fw_SPECT, sizeof(fw_SPECT),
+                                                           FW_SPECT_UPDATE_BANK)));  // Update SPECT firmware bank
     }
     else {
-        LT_LOG("     ERROR device couldn't get into MAINTENANCE mode");
+        LT_LOG_INFO("     ERROR device couldn't get into MAINTENANCE mode");
         return -1;
     }
     LT_LOG_LINE();
@@ -96,25 +102,27 @@ int lt_ex_fw_update(lt_handle_t *h)
     if (h->l2.mode == LT_MODE_APP) {
         ret = lt_get_info_riscv_fw_ver(h, fw_ver, LT_L2_GET_INFO_RISCV_FW_SIZE);
         if (ret == LT_OK) {
-            LT_LOG("  Chip is executing RISCV application firmware version: %d.%d.%d    (+ .%d)", fw_ver[3], fw_ver[2],
-                   fw_ver[1], fw_ver[0]);
+            LT_LOG_INFO("  Chip is executing RISCV application firmware version: %d.%d.%d    (+ .%d)", fw_ver[3],
+                        fw_ver[2], fw_ver[1], fw_ver[0]);
         }
         else {
             LT_LOG_ERROR("Failed to get RISCV firmware version, ret=%s", lt_ret_verbose(ret));
+            return -1;
         }
 
         // Getting SPECT firmware version
         ret = lt_get_info_spect_fw_ver(h, fw_ver, LT_L2_GET_INFO_SPECT_FW_SIZE);
         if (ret == LT_OK) {
-            LT_LOG("  Chip is executing SPECT firmware version:             %d.%d.%d    (+ .%d)", fw_ver[3], fw_ver[2],
-                   fw_ver[1], fw_ver[0]);
+            LT_LOG_INFO("  Chip is executing SPECT firmware version:             %d.%d.%d    (+ .%d)", fw_ver[3],
+                        fw_ver[2], fw_ver[1], fw_ver[0]);
         }
         else {
             LT_LOG_ERROR("Failed to get SPECT firmware version, ret=%s", lt_ret_verbose(ret));
+            return -1;
         }
     }
     else {
-        LT_LOG(
+        LT_LOG_INFO(
             "     Device couldn't get into APP mode, APP and SPECT firmwares in fw banks are not valid or banks are "
             "empty");
     }
--- examples/lt_ex_show_chip_id_and_fwver.c
+++ examples/lt_ex_show_chip_id_and_fwver.c
@@ -21,46 +21,46 @@ static void print_header_boot_v1_0_1(uint8_t *data, bank_id_t bank_id)
     struct header_boot_v1_t *p_h = (struct header_boot_v1_t *)data;
     switch (bank_id) {
         case FW_BANK_FW1:
-            LT_LOG("    Firmware bank 1 header:");
+            LT_LOG_INFO("    Firmware bank 1 header:");
             break;
         case FW_BANK_FW2:
-            LT_LOG("    Firmware bank 2 header:");
+            LT_LOG_INFO("    Firmware bank 2 header:");
             break;
         case FW_BANK_SPECT1:
-            LT_LOG("    SPECT bank 1 header:");
+            LT_LOG_INFO("    SPECT bank 1 header:");
             break;
         case FW_BANK_SPECT2:
-            LT_LOG("    SPECT bank 2 header:");
+            LT_LOG_INFO("    SPECT bank 2 header:");
             break;
         default:
-            LT_LOG_ERROR("Unknown bank ID: %d", bank_id);
+            LT_LOG_ERROR("Unknown bank ID: %d", (int)bank_id);
             return;
     }
-    LT_LOG("      Type:                           %02X%02X%02X%02X", p_h->type[3], p_h->type[2], p_h->type[1],
-           p_h->type[0]);
-    LT_LOG("      Version:                        %02X%02X%02X%02X", p_h->version[3], p_h->version[2], p_h->version[1],
-           p_h->version[0]);
-    LT_LOG("      Size:                           %02X%02X%02X%02X", p_h->size[3], p_h->size[2], p_h->size[1],
-           p_h->size[0]);
-    LT_LOG("      Git hash:                       %02X%02X%02X%02X", p_h->git_hash[3], p_h->git_hash[2],
-           p_h->git_hash[1], p_h->git_hash[0]);
-    LT_LOG("      FW hash:                        %02X%02X%02X%02X", p_h->hash[3], p_h->hash[2], p_h->hash[1],
-           p_h->hash[0]);
+    LT_LOG_INFO("      Type:                           %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8, p_h->type[3],
+                p_h->type[2], p_h->type[1], p_h->type[0]);
+    LT_LOG_INFO("      Version:                        %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8, p_h->version[3],
+                p_h->version[2], p_h->version[1], p_h->version[0]);
+    LT_LOG_INFO("      Size:                           %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8, p_h->size[3],
+                p_h->size[2], p_h->size[1], p_h->size[0]);
+    LT_LOG_INFO("      Git hash:                       %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8, p_h->git_hash[3],
+                p_h->git_hash[2], p_h->git_hash[1], p_h->git_hash[0]);
+    LT_LOG_INFO("      FW hash:                        %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8, p_h->hash[3],
+                p_h->hash[2], p_h->hash[1], p_h->hash[0]);
 }
 
 static void print_all_headers_v1(lt_handle_t *h)
 {
-    LT_LOG("  Reading firmware headers in all banks...");
+    LT_LOG_INFO("  Reading firmware headers in all banks...");
     uint8_t header[LT_L2_GET_INFO_FW_HEADER_SIZE] = {0};
 
     // Read header from FW_BANK_FW1
-    memset(header, 0, sizeof(header));
     lt_ret_t ret = lt_get_info_fw_bank(h, FW_BANK_FW1, header, sizeof(header));
     if (ret == LT_OK) {
         print_header_boot_v1_0_1(header, FW_BANK_FW1);
     }
     else {
         LT_LOG_ERROR("Failed to get FW bank 1 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_FW2
@@ -71,6 +71,7 @@ static void print_all_headers_v1(lt_handle_t *h)
     }
     else {
         LT_LOG_ERROR("Failed to get FW bank 2 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_SPECT1
@@ -81,6 +82,7 @@ static void print_all_headers_v1(lt_handle_t *h)
     }
     else {
         LT_LOG_ERROR("Failed to get SPECT bank 1 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_SPECT2
@@ -91,95 +93,107 @@ static void print_all_headers_v1(lt_handle_t *h)
     }
     else {
         LT_LOG_ERROR("Failed to get SPECT bank 2 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 }
 
 // This function prints the header in the new format used in bootloader version 2.0.1
-static void print_header_v2(void *data, bank_id_t bank_id)
+static void print_header_boot_v2_0_1(void *data, bank_id_t bank_id)
 {
     struct header_boot_v2_t *p_h = (struct header_boot_v2_t *)data;
     switch (bank_id) {
         case FW_BANK_FW1:
-            LT_LOG("    Firmware bank 1 header:");
+            LT_LOG_INFO("    Firmware bank 1 header:");
             break;
         case FW_BANK_FW2:
-            LT_LOG("    Firmware bank 2 header:");
+            LT_LOG_INFO("    Firmware bank 2 header:");
             break;
         case FW_BANK_SPECT1:
-            LT_LOG("    SPECT bank 1 header:");
+            LT_LOG_INFO("    SPECT bank 1 header:");
             break;
         case FW_BANK_SPECT2:
-            LT_LOG("    SPECT bank 2 header:");
+            LT_LOG_INFO("    SPECT bank 2 header:");
             break;
         default:
-            LT_LOG_ERROR("Unknown bank ID: %d", bank_id);
+            LT_LOG_ERROR("Unknown bank ID: %d", (int)bank_id);
             return;
     }
-    LT_LOG("      Type:                           %04X", p_h->type);
-    LT_LOG("      Padding:                        %02X", p_h->padding);
-    LT_LOG("      FW header version:              %02X", p_h->header_version);
-    LT_LOG("      Version:                        %08" PRIX32, p_h->ver);
-    LT_LOG("      Size:                           %08" PRIX32, p_h->size);
-    LT_LOG("      Git hash:                       %08" PRIX32, p_h->git_hash);
+    LT_LOG_INFO("      Type:                           %04" PRIX16, p_h->type);
+    LT_LOG_INFO("      Padding:                        %02" PRIX8, p_h->padding);
+    LT_LOG_INFO("      FW header version:              %02" PRIX8, p_h->header_version);
+    LT_LOG_INFO("      Version:                        %08" PRIX32, p_h->ver);
+    LT_LOG_INFO("      Size:                           %08" PRIX32, p_h->size);
+    LT_LOG_INFO("      Git hash:                       %08" PRIX32, p_h->git_hash);
     // Hash str has 32B
     char hash_str[32 * 2 + 1] = {0};
     for (int i = 0; i < 32; i++) {
         snprintf(hash_str + i * 2, sizeof(hash_str) - i * 2, "%02X", p_h->hash[i]);
     }
-    LT_LOG("      Hash:                           %s", hash_str);
-    LT_LOG("      Pair version:                   %08" PRIX32, p_h->pair_version);
+    LT_LOG_INFO("      Hash:                           %s", hash_str);
+    LT_LOG_INFO("      Pair version:                   %08" PRIX32, p_h->pair_version);
 }
 
 static void print_all_fw_headers_v2(lt_handle_t *h)
 {
-    LT_LOG("  Reading firmware headers in all banks...");
+    LT_LOG_INFO("  Reading firmware headers in all banks...");
     uint8_t header[LT_L2_GET_INFO_FW_HEADER_SIZE] = {0};
 
     // Read header from FW_BANK_FW1
     lt_ret_t ret = lt_get_info_fw_bank(h, FW_BANK_FW1, header, sizeof(header));
     if (ret == LT_OK) {
-        print_header_v2(header, FW_BANK_FW1);
+        print_header_boot_v2_0_1(header, FW_BANK_FW1);
     }
     else {
         LT_LOG_ERROR("Failed to get FW bank 1 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_FW2
     memset(header, 0, sizeof(header));
     ret = lt_get_info_fw_bank(h, FW_BANK_FW2, header, sizeof(header));
     if (ret == LT_OK) {
-        print_header_v2(header, FW_BANK_FW2);
+        print_header_boot_v2_0_1(header, FW_BANK_FW2);
     }
     else {
         LT_LOG_ERROR("Failed to get FW bank 2 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_SPECT1
     memset(header, 0, sizeof(header));
     ret = lt_get_info_fw_bank(h, FW_BANK_SPECT1, header, sizeof(header));
     if (ret == LT_OK) {
-        print_header_v2(header, FW_BANK_SPECT1);
+        print_header_boot_v2_0_1(header, FW_BANK_SPECT1);
     }
     else {
         LT_LOG_ERROR("Failed to get SPECT bank 1 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 
     // Read header from FW_BANK_SPECT2
     memset(header, 0, sizeof(header));
     ret = lt_get_info_fw_bank(h, FW_BANK_SPECT2, header, sizeof(header));
     if (ret == LT_OK) {
-        print_header_v2(header, FW_BANK_SPECT2);
+        print_header_boot_v2_0_1(header, FW_BANK_SPECT2);
     }
     else {
         LT_LOG_ERROR("Failed to get SPECT bank 2 header, ret=%s", lt_ret_verbose(ret));
+        return;
     }
 }
 
+/**
+ * @brief This example shows how to read TROPIC01's chip ID and firmware versions. Prints chip application firmware
+ * versions and also bootloader version.
+ *
+ * @param h
+ * @return int
+ */
 int lt_ex_show_chip_id_and_fwver(lt_handle_t *h)
 {
-    LT_LOG("\t=======================================================================");
-    LT_LOG("\t=====  TROPIC01 explore chip                                        ===");
-    LT_LOG("\t=======================================================================");
+    LT_LOG_INFO("\t=======================================================================");
+    LT_LOG_INFO("\t=====  TROPIC01 show chip ID and firmware versions                  ===");
+    LT_LOG_INFO("\t=======================================================================");
 
     // This variable is reused on more places in this example to store different firmware versions
     uint8_t fw_ver[LT_L2_GET_INFO_RISCV_FW_SIZE] = {0};
@@ -187,16 +201,19 @@ int lt_ex_show_chip_id_and_fwver(lt_handle_t *h)
     lt_ret_t ret = lt_init(h);
     if (ret != LT_OK) {
         LT_LOG_ERROR("Failed to initialize handle, ret=%s", lt_ret_verbose(ret));
+        lt_deinit(h);
         return -1;
     }
 
     // First we check versions of both updateable firmwares. For this we need to be in APPLICATION mode.
     // If there are valid firmwares, chip will execute them on boot. In any case we will try to reboot into application,
     // in case chip would be in maintenance mode (executing bootloader)
-    LT_LOG("  Rebooting into APPLICATION mode to check firmware versions...");
+    LT_LOG_INFO("  Rebooting into APPLICATION mode to check firmware versions...");
     ret = lt_reboot(h, LT_MODE_APP);
     if (ret != LT_OK) {
         LT_LOG_ERROR("lt_reboot() failed, ret=%s", lt_ret_verbose(ret));
+        lt_deinit(h);
+        return -1;
     }
 
     if (h->l2.mode == LT_MODE_APP) {
@@ -204,30 +221,36 @@ int lt_ex_show_chip_id_and_fwver(lt_handle_t *h)
         // Getting RISCV app firmware version
         ret = lt_get_info_riscv_fw_ver(h, fw_ver, LT_L2_GET_INFO_RISCV_FW_SIZE);
         if (ret == LT_OK) {
-            LT_LOG("  Chip is executing RISCV application firmware version: %d.%d.%d    (+ .%d)", fw_ver[3], fw_ver[2],
-                   fw_ver[1], fw_ver[0]);
+            LT_LOG_INFO("  Chip is executing RISCV application firmware version: %02" PRIX8 ".%02" PRIX8 ".%02" PRIX8
+                        "    (+ .%02" PRIX8 ")",
+                        fw_ver[3], fw_ver[2], fw_ver[1], fw_ver[0]);
         }
         else {
             LT_LOG_ERROR("Failed to get RISCV firmware version, ret=%s", lt_ret_verbose(ret));
+            lt_deinit(h);
+            return -1;
         }
 
         // Getting SPECT firmware version
         ret = lt_get_info_spect_fw_ver(h, fw_ver, LT_L2_GET_INFO_SPECT_FW_SIZE);
         if (ret == LT_OK) {
-            LT_LOG("  Chip is executing SPECT firmware version:             %d.%d.%d    (+ .%d)", fw_ver[3], fw_ver[2],
-                   fw_ver[1], fw_ver[0]);
+            LT_LOG_INFO("  Chip is executing SPECT firmware version:             %02" PRIX8 ".%02" PRIX8 ".%02" PRIX8
+                        "    (+ .%02" PRIX8 ")",
+                        fw_ver[3], fw_ver[2], fw_ver[1], fw_ver[0]);
         }
         else {
             LT_LOG_ERROR("Failed to get SPECT firmware version, ret=%s", lt_ret_verbose(ret));
+            lt_deinit(h);
+            return -1;
         }
     }
     else {
-        LT_LOG("     ERROR device couldn't get into APP mode, APP and SPECT firmwares in fw banks are not valid");
+        LT_LOG_ERROR("Device couldn't get into APP mode, APP and SPECT firmwares in fw banks are not valid");
     }
 
     LT_LOG_LINE();
 
-    LT_LOG("  Rebooting into MAINTENANCE mode to check bootloader version and fw bank headers...");
+    LT_LOG_INFO("  Rebooting into MAINTENANCE mode to check bootloader version and fw bank headers...");
     ret = lt_reboot(h, LT_MODE_MAINTENANCE);
     if (ret != LT_OK) {
         LT_LOG_ERROR("Failed to reboot into MAINTENANCE mode, ret=%s", lt_ret_verbose(ret));
@@ -245,39 +268,56 @@ int lt_ex_show_chip_id_and_fwver(lt_handle_t *h)
 
         // Checking if bootloader version is 1.0.1
         if (((fw_ver[3] & 0x7f) == 1) && (fw_ver[2] == 0) && (fw_ver[1] == 1) && (fw_ver[0] == 0)) {
-            LT_LOG("  Bootloader version:                 %d.%d.%d    (+ .%d)", fw_ver[3] & 0x7f, fw_ver[2], fw_ver[1],
-                   fw_ver[0]);
+            LT_LOG_INFO("  Bootloader version:                 %02" PRIX8 ".%02" PRIX8 ".%02" PRIX8 "    (+ .%02" PRIX8
+                        ")",
+                        fw_ver[3] & 0x7f, fw_ver[2], fw_ver[1], fw_ver[0]);
 
             print_all_headers_v1(h);
         }
         else {
             // Checking if bootloader version is 2.0.1
             if (((fw_ver[3] & 0x7f) == 2) && (fw_ver[2] == 0) && (fw_ver[1] == 1) && (fw_ver[0] == 0)) {
-                LT_LOG("  Bootloader version:                 %d.%d.%d    (+ .%d)", fw_ver[3] & 0x7f, fw_ver[2],
-                       fw_ver[1], fw_ver[0]);
+                LT_LOG_INFO("  Bootloader version:                 %02" PRIX8 ".%02" PRIX8 ".%02" PRIX8
+                            "    (+ .%02" PRIX8 ")",
+                            fw_ver[3] & 0x7f, fw_ver[2], fw_ver[1], fw_ver[0]);
                 print_all_fw_headers_v2(h);
             }
             else {
-                LT_LOG_ERROR("Unknown bootloader version %d.%d.%d.%d", fw_ver[3] & 0x7f, fw_ver[2], fw_ver[1],
-                             fw_ver[0]);
+                LT_LOG_ERROR("Unknown bootloader version %02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8,
+                             fw_ver[3] & 0x7f, fw_ver[2], fw_ver[1], fw_ver[0]);
             }
             LT_LOG_LINE();
         }
     }
     else {
-        LT_LOG("     ERROR device couldn't get into MAINTENANCE mode");
+        LT_LOG_INFO("     ERROR device couldn't get into MAINTENANCE mode");
         lt_deinit(h);
         return -1;
     }
 
     LT_LOG_LINE();
 
-    LT_LOG("Reading Chip ID:");
+    LT_LOG_INFO("Reading Chip ID:");
 
     struct lt_chip_id_t chip_id = {0};
 
-    lt_get_info_chip_id(h, &chip_id);
-    lt_print_chip_id(&chip_id, printf);
+    ret = lt_get_info_chip_id(h, &chip_id);
+    if (ret != LT_OK) {
+        LT_LOG_ERROR("Failed to get chip ID, ret=%s", lt_ret_verbose(ret));
+        lt_deinit(h);
+        return -1;
+    }
+
+    ret = lt_print_chip_id(&chip_id, printf);
+    if (ret != LT_OK) {
+        LT_LOG_ERROR("Failed to print chip ID, ret=%s", lt_ret_verbose(ret));
+        lt_deinit(h);
+        return -1;
+    }
+
+    LT_LOG_LINE();
+
+    lt_deinit(h);
 
     return 0;
 }
