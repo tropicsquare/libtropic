f82fd787cdf03484b841eeb67e3829995188d94e
andreondra <golasowski.o@gmail.com>
2025-09-24 08:02:29 +0200

    doc: added TODO to Errata number

--- src/libtropic_l2.c
+++ src/libtropic_l2.c
@@ -68,7 +68,7 @@ lt_ret_t lt_l2_receive(lt_l2_state_t *s2)
     }
 
     // Fix of the chip FW bug, where several last bits of the frame may be missing
-    // if the chip started to reboot. See errata xy.
+    // if the chip started to reboot. See Errata TODO NUMBER.
     //
     // If the reboot was successful, we only check the frame up to the first CRC frame.
     if (s2->startup_req_sent && s2->buff[1] == 0x01 && s2->buff[2] == 0x00 && s2->buff[3] == 0x03) {
