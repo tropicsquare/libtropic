b1f21e5a431f121323a6f55bfae5b7cdf3261101
andreondra <golasowski.o@gmail.com>
2025-09-29 14:54:24 +0200

    refactor(lt_test_rev_startup_req): use correct terminology, use new macros

--- tests/functional/lt_test_rev_startup_req.c
+++ tests/functional/lt_test_rev_startup_req.c
@@ -19,9 +19,9 @@
 #define REBOOT_WAIT_ATTEMPTS 10
 
 lt_handle_t *g_h;
-enum lt_tropic01_mode { STARTUP_MODE, APPLICATION_MODE, BUSY };
+enum lt_current_chip_state { MAINTENANCE_MODE, APPLICATION_MODE, CHIP_BUSY };
 
-static enum lt_tropic01_mode check_current_mode(void)
+static enum lt_current_chip_state check_current_state(void)
 {
     uint8_t spect_ver[TR01_L2_GET_INFO_SPECT_FW_SIZE];
     lt_ret_t ret;
@@ -43,13 +43,13 @@ static enum lt_tropic01_mode check_current_mode(void)
     }
     else {
         LT_LOG_ERROR("Chip still busy! Terminating test.");
-        return BUSY;
+        return CHIP_BUSY;
     }
 
     LT_LOG_INFO("Spect version:");
     hexdump_8byte(spect_ver, TR01_L2_GET_INFO_SPECT_FW_SIZE);
     if (0 == memcmp(spect_ver, "\x00\x00\x00\x80", TR01_L2_GET_INFO_SPECT_FW_SIZE)) {
-        return STARTUP_MODE;
+        return MAINTENANCE_MODE;
     }
     else {
         return APPLICATION_MODE;
@@ -60,16 +60,16 @@ static lt_ret_t lt_test_rev_startup_req_cleanup(void)
 {
     lt_ret_t ret;
 
-    LT_LOG_INFO("Rebooting to the normal mode...");
-    ret = lt_reboot(g_h, TR01_L2_STARTUP_REQ_STARTUP_ID_REBOOT);
+    LT_LOG_INFO("Rebooting to the app mode...");
+    ret = lt_reboot(g_h, TR01_REBOOT);
     if (LT_OK != ret) {
-        LT_LOG_ERROR("Couldn't reboot to the normal mode!");
+        LT_LOG_ERROR("Couldn't reboot to the app mode!");
         return ret;
     }
 
-    LT_LOG_INFO("Checking we are again in the normal mode...");
-    if (APPLICATION_MODE != check_current_mode()) {
-        LT_LOG_ERROR("Still in bootloader mode!");
+    LT_LOG_INFO("Checking the chip is in the app mode...");
+    if (APPLICATION_MODE != check_current_state()) {
+        LT_LOG_ERROR("Still in maintenance mode!");
         return LT_FAIL;
     }
 
@@ -90,20 +90,21 @@ void lt_test_rev_startup_req(lt_handle_t *h)
 
     lt_test_cleanup_function = &lt_test_rev_startup_req_cleanup;
 
-    // Part 1: Try to reboot from normal to normal.
-    LT_LOG_INFO("Checking we are in the normal mode...");
-    LT_TEST_ASSERT(APPLICATION_MODE, check_current_mode());
-    LT_LOG_INFO("Rebooting to the normal mode...");
-    LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_L2_STARTUP_REQ_STARTUP_ID_REBOOT));
-    LT_LOG_INFO("Checking we are again in the normal mode...");
-    LT_TEST_ASSERT(APPLICATION_MODE, check_current_mode());
-
-    // Part 2: Try to reboot from normal to bootloader and from bootloader to bootloader.
+    // Part 1: Try to reboot from app mode to app mode twice. First time to make sure we are in 
+    // the app mode, second time to try rebooting to the app mode from the app mode.
     for (int i = 0; i < 2; i++) {
-        LT_LOG_INFO("Rebooting to the bootloader mode (maintenance reboot)...");
-        LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_L2_STARTUP_REQ_STARTUP_ID_MAINTENANCE_REBOOT));
-        LT_LOG_INFO("Checking we are in the bootloader mode...");
-        LT_TEST_ASSERT(STARTUP_MODE, check_current_mode());
+        LT_LOG_INFO("Rebooting to the app mode...");
+        LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_REBOOT));
+        LT_LOG_INFO("Checking the chip is in the normal mode...");
+        LT_TEST_ASSERT(APPLICATION_MODE, check_current_state());
+    }
+
+    // Part 2: Try to reboot from app to maintenance mode and from maintenance to maintenance mode.
+    for (int i = 0; i < 2; i++) {
+        LT_LOG_INFO("Rebooting to the maintenance mode (maintenance reboot)...");
+        LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_MAINTENANCE_REBOOT));
+        LT_LOG_INFO("Checking the chip is in the maintenance mode...");
+        LT_TEST_ASSERT(MAINTENANCE_MODE, check_current_state());
         LT_LOG_INFO("Checking that the handshake does not work...");
 #ifdef ABAB
         LT_TEST_ASSERT(LT_L2_GEN_ERR,
@@ -117,9 +118,9 @@ void lt_test_rev_startup_req(lt_handle_t *h)
 #endif
     }
 
-    // Part 3: Try to reboot from bootloader to normal.
-    LT_LOG_INFO("Rebooting to the normal mode...");
-    LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_L2_STARTUP_REQ_STARTUP_ID_REBOOT));
-    LT_LOG_INFO("Checking we are again in the normal mode...");
-    LT_TEST_ASSERT(APPLICATION_MODE, check_current_mode());
+    // Part 3: Try to reboot from maintenance mode to app mode.
+    LT_LOG_INFO("Rebooting to the app mode...");
+    LT_TEST_ASSERT(LT_OK, lt_reboot(h, TR01_REBOOT));
+    LT_LOG_INFO("Checking the chip is in the app mode...");
+    LT_TEST_ASSERT(APPLICATION_MODE, check_current_state());
 }
