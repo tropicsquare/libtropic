ff0f24fdb2a4de8762cce2bd72c3a0105ebbf2ee
Jan Medek <czmedexs@gmail.com>
2025-10-03 19:12:45 +0200

    feat(arduino): implement minimal port

---
+++ hal/port/arduino/libtropic_port_arduino.h
@@ -0,0 +1,31 @@
+#ifndef LIBTROPIC_PORT_ARDUINO_H
+#define LIBTROPIC_PORT_ARDUINO_H
+
+/**
+ * @file libtropic_port_arduino.h
+ * @author Tropic Square s.r.o.
+ * @brief Port for the Arduino framework.
+ *
+ * @license For the license see file LICENSE.txt file in the root directory of this source tree.
+ */
+
+#include <Arduino.h>
+#include <SPI.h>
+
+#include "libtropic_port.h"
+
+/**
+ * @brief TODO
+ *
+ */
+typedef struct lt_dev_arduino_t {
+    /** @public @brief TODO */
+    uint8_t cs_pin;
+    SPISettings spi_settings;
+    unsigned int rng_seed;
+
+    /** @private @brief TODO */
+    SPIClass *spi;
+} lt_dev_arduino_t;
+
+#endif  // LIBTROPIC_PORT_ARDUINO_H
