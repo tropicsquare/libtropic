b9242e20b1aa18110f9a4f6600ef25ea2006de0c
Jan Medek <czmedexs@gmail.com>
2025-11-12 15:54:41 +0100

    refactor(static_asan_flags.cmake): put flags into a separate variable

--- cmake/static_asan_flags.cmake
+++ cmake/static_asan_flags.cmake
@@ -1,6 +1,17 @@
 # This file defines an INTERFACE library that carries all of our
 # project-wide compile and link flags for static ASan.
 
+set(LT_ASAN_COMPILE_FLAGS
+    "-fsanitize=address,leak,undefined"
+    "-fno-omit-frame-pointer"
+    "-g"
+    CACHE STRING "Compile flags for Static ASan")
+
+set(LT_ASAN_LINK_FLAGS
+    "-fsanitize=address,leak,undefined"
+    "-static-libasan"
+    CACHE STRING "Link flags for Static ASan")
+
 # Create the INTERFACE library with the requested name.
 add_library(libtropic_static_asan_flags INTERFACE)
 
@@ -11,12 +22,9 @@ add_library(libtropic::static_asan_flags ALIAS libtropic_static_asan_flags)
 # Add the desired compile options to its INTERFACE.
 # Any target that links to this will inherit these flags.
 target_compile_options(libtropic_static_asan_flags INTERFACE
-    -fsanitize=address,leak,undefined
-    -fno-omit-frame-pointer
-    -g
+    ${LT_ASAN_COMPILE_FLAGS}
 )
 
 target_link_options(libtropic_static_asan_flags INTERFACE
-    -fsanitize=address,leak,undefined
-    -static-libasan
+    ${LT_ASAN_LINK_FLAGS}
 )
