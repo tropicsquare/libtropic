b01d2f074bca88fb3411b03f37895511f3dae2ff
Jan Medek <czmedexs@gmail.com>
2025-11-18 13:13:57 +0100

    refactor(lt_tr01_attrs): add doxygen headers for files, attribute for unused result

--- src/lt_tr01_attrs.c
+++ src/lt_tr01_attrs.c
@@ -1,3 +1,10 @@
+/**
+ * @file lt_tr01_attrs.c
+ * @brief Implementation for handling TROPIC01 attributes based on FW versions.
+ * @copyright Copyright (c) 2020-2025 Tropic Square s.r.o.
+ *
+ * @license For the license see file LICENSE.txt file in the root directory of this source tree.
+ */
 
 #include "lt_tr01_attrs.h"
 
--- src/lt_tr01_attrs.h
+++ src/lt_tr01_attrs.h
@@ -1,6 +1,14 @@
 #ifndef LT_TR01_ATTRS_H
 #define LT_TR01_ATTRS_H
 
+/**
+ * @file lt_tr01_attrs.h
+ * @brief Declarations for handling TROPIC01 attributes based on FW versions.
+ * @copyright Copyright (c) 2020-2025 Tropic Square s.r.o.
+ *
+ * @license For the license see file LICENSE.txt file in the root directory of this source tree.
+ */
+
 #include <stdint.h>
 
 #include "libtropic_common.h"
@@ -12,6 +20,6 @@
  * @retval    LT_OK Function executed successfully
  * @retval    other Function did not execute successully, you might use lt_ret_verbose() to get verbose encoding
  */
-lt_ret_t lt_init_tr01_attrs(lt_handle_t *h);
+lt_ret_t lt_init_tr01_attrs(lt_handle_t *h) __attribute__((warn_unused_result));
 
 #endif  // LT_TR01_ATTRS_H
