ef5882fdb65f8658deb93c1d89d942c3e79b5ef6
Ondrej Golasowski <golasowski.o@gmail.com>
2025-12-02 16:28:09 +0100

    doc: updated Arduino HAL copyright notice

--- hal/arduino/libtropic_port_arduino.cpp
+++ hal/arduino/libtropic_port_arduino.cpp
@@ -1,6 +1,6 @@
 /**
- * @file libtropic_port_arduino.c
- * @author Tropic Square s.r.o.
+ * @file libtropic_port_arduino.cpp
+ * @copyright Copyright (c) 2020-2025 Tropic Square s.r.o.
  * @brief Port for the Arduino framework.
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
