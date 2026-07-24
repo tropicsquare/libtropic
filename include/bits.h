91e127dc561680253643d096929fc2fc3fcd58ff
Jan Medek <czmedexs@gmail.com>
2026-07-24 16:24:18 +0200

    refactor(bits.h): use __typeof__ instead of typeof to satisfy -Wlanguage-extension-token when using Clang on MacOS
    - __typeof__ already used in the `lt_max`, so it's also a consistency update

--- include/bits.h
+++ include/bits.h
@@ -80,9 +80,9 @@ extern "C" {
  *    "({" "})" braces were removed since they are non-ISO C.
  */
 
-#define FIELD_GET(_mask, _reg) ((typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask)))
+#define FIELD_GET(_mask, _reg) ((__typeof__(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask)))
 
-#define FIELD_PREP(_mask, _val) (((typeof(_mask))(_val) << __bf_shf(_mask)) & (_mask))
+#define FIELD_PREP(_mask, _val) (((__typeof__(_mask))(_val) << __bf_shf(_mask)) & (_mask))
 
 #define FIELD_SET(_reg, _mask, _val)     \
     {                                    \
